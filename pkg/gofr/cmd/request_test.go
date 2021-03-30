d5e6370f966d52155f5f177a7fdbea2bf2b2de97
Vikash Kumar <vikash.iitb@gmail.com>
2021-03-30 19:57:02 +0530

    Adding tests for cmd requests

---
+++ pkg/gofr/cmd/request_test.go
@@ -0,0 +1,31 @@
+package cmd
+
+import "testing"
+
+func TestRequest_Bind(t *testing.T) {
+	r := NewRequest([]string{"command", "-Name=gofr", "-Valid=true", "-Value=12", "-test"}) // TODO: Only fields starting with Capital letter can be 'bind' right now.
+
+	if r.Param("Name") != "gofr" {
+		t.Error("Param parse error.")
+	}
+
+	if r.Param("test") != "true" {
+		t.Error("Param parse error.")
+	}
+
+	if r.PathParam("Value") != "12" {
+		t.Error("PathParam error.")
+	}
+
+	// Testing string, bool, int
+	a := struct {
+		Name  string
+		Valid bool
+		Value int
+	}{}
+
+	r.Bind(&a)
+	if a.Name != "gofr" || a.Valid != true || a.Value != 12 {
+		t.Errorf("1. Request Bind error. Got: %v", a)
+	}
+}
