c8f5c3c726bd65c6df637ef9ed3d2e7276209e6b
mehrotra234 <aryanmehrotra2000@gmail.com>
2023-11-23 15:29:27 +0530

    add navigation.js

---
+++ docs/navigation.js
@@ -0,0 +1,43 @@
+export const navigation = [
+    {
+        title: 'Quick Start Guide',
+        links: [
+            { title: 'Hello Server', href: '/docs/v1/quick-start/introduction' },
+            { title: 'Configuration', href: '/docs/v1/quick-start/configuration' },
+            { title: 'Connecting Redis', href: '/docs/v1/quick-start/connecting-redis' },
+            { title: 'Connecting MySQL', href: '/docs/v1/quick-start/connecting-mysql' },
+            { title: 'Observability', href: '/docs/v1/quick-start/observability' },
+        ],
+    },
+    {
+        title: 'Advanced Guide',
+        links: [
+            { title: 'Middlewares', href: '/docs/v1/advanced-guide/middleware' },
+            { title: 'Publishing Custom Metrics', href: '/docs/v1/advanced-guide/pushing-custom-metrics' },
+            { title: 'Handling Data Migrations', href: '/docs/v1/advanced-guide/data-migrations' },
+            { title: 'Dealing with Remote Files', href: '/docs/v1/advanced-guide/remote-files' },
+            { title: 'Dynamic Configurations', href: '/docs/v1/advanced-guide/dynamic-configs' },
+            { title: 'Supporting OAuth', href: '/docs/v1/advanced-guide/oauth' },
+            // { title: 'Circuit Breaker Support', href: '/docs/v1/advanced-guide/circuit-breaker' },
+            { title: 'Writing gRPC Server', href: '/docs/v1/advanced-guide/grpc' },
+            { title: 'Creating a Static File Server', href: '/docs/v1/advanced-guide/static-file-server' },
+            { title: 'WebSockets', href: '/docs/v1/advanced-guide/websockets' },
+        ],
+    },
+    {
+        title: 'References',
+        links: [
+            { title: 'Context', href: '/docs/v1/references/context' },
+            { title: 'Configuration', href: '/docs/v1/references/configs' },
+            { title: 'HTTP Service', href: '/docs/v1/references/http-service' },
+            { title: 'Files', href: '/docs/v1/references/files' },
+            { title: 'Datastore', href: '/docs/v1/references/datastore' },
+            { title: 'PubSub', href: '/docs/v1/references/pubsub' },
+            { title: 'Metrics', href: '/docs/v1/references/metrics' },
+            { title: 'Traces', href: '/docs/v1/references/traces' },
+            { title: 'Logs', href: '/docs/v1/references/logs' },
+            { title: 'Errors', href: '/docs/v1/references/errors' },
+            { title: 'Swaggger', href: '/docs/v1/references/swagger' },
+        ],
+    },
+]
