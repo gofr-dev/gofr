57243c7e01839fc3e23a2d5a95cf897465568048
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2023-12-27 11:26:34 +0530

    remove parallel run

--- cmd/gofr/migration/migrate_test.go
+++ cmd/gofr/migration/migrate_test.go
@@ -424,12 +424,9 @@ func Test_DirtyTest(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.dbDriver.Run(nil, tt.args.app, tt.args.method, tt.args.name, logger); (err != nil) != tt.wantErr {
-				t.Errorf("preRun() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
+		if err := tt.dbDriver.Run(nil, tt.args.app, tt.args.method, tt.args.name, logger); (err != nil) != tt.wantErr {
+			t.Errorf("preRun() error = %v, wantErr %v", err, tt.wantErr)
+		}
 	}
 }
 
