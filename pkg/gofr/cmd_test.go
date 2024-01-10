b42e15fcca5e3954316076dad611e71bc55ffb2b
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-01-11 01:23:04 +0530

    refactor cases

--- pkg/gofr/cmd_test.go
+++ pkg/gofr/cmd_test.go
@@ -100,6 +100,34 @@ func Test_Run_SuccessRouteWithSpecialCharacters(t *testing.T) {
 	}
 }
 
+func Test_Run_ErrorRouteWithSpecialCharacters(t *testing.T) {
+	testCases := []struct {
+		desc string
+		args []string
+	}{
+		{"special character $", []string{"", "command-with-special-characters$"}},
+		{"special character ^", []string{"", "command-with-special-characters^"}},
+	}
+
+	for i, tc := range testCases {
+		os.Args = tc.args
+		c := cmd{}
+
+		c.addRoute(tc.args[1], func(c *Context) (interface{}, error) {
+			c.Logger.Info("handler called")
+
+			return nil, nil
+		})
+
+		logs := testutil.StderrOutputForFunc(func() {
+			c.Run(container.NewContainer(config.NewEnvFile("")))
+		})
+
+		assert.NotContains(t, logs, "handler called", "TEST[%d] Failed.\n %s", i, tc.desc)
+		assert.Contains(t, logs, "No Command Found!", "TEST[%d] Failed.\n %s", i, tc.desc)
+	}
+}
+
 func Test_Run_ErrorParamNotReadWithoutHyphen(t *testing.T) {
 	os.Args = []string{"", "log", "hello=world"}
 
@@ -121,18 +149,6 @@ func Test_Run_ErrorParamNotReadWithoutHyphen(t *testing.T) {
 
 func Test_Run_ErrorNotARegisteredCommand(t *testing.T) {
 	os.Args = []string{"", "log"}
-
-	c := cmd{}
-
-	logs := testutil.StderrOutputForFunc(func() {
-		c.Run(container.NewContainer(config.NewEnvFile("")))
-	})
-
-	assert.Contains(t, logs, "No Command Found!")
-}
-
-func Test_Run_ErrorNoArgumentGiven(t *testing.T) {
-	os.Args = []string{""}
 
 	c := cmd{}
 
@@ -168,6 +184,18 @@ func Test_Run_ErrorRouteRegisteredButNilHandler(t *testing.T) {
 	c := cmd{}
 
 	c.addRoute("route", nil)
+
+	logs := testutil.StderrOutputForFunc(func() {
+		c.Run(container.NewContainer(config.NewEnvFile("")))
+	})
+
+	assert.Contains(t, logs, "No Command Found!")
+}
+
+func Test_Run_ErrorNoArgumentGiven(t *testing.T) {
+	os.Args = []string{""}
+
+	c := cmd{}
 
 	logs := testutil.StderrOutputForFunc(func() {
 		c.Run(container.NewContainer(config.NewEnvFile("")))
