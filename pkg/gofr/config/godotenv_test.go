cd2b7f4fa7d9021d0e6da7bf144cdb0d41ebf3f9
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-01-23 00:29:06 +0530

    refactor test

--- pkg/gofr/config/godotenv_test.go
+++ pkg/gofr/config/godotenv_test.go
@@ -1,48 +1,76 @@
 package config
 
 import (
-	"testing"
-
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
 )
 
-func TestNewEnvFile(t *testing.T) {
-	resp := NewEnvFile("/configs")
-
-	assert.IsType(t, &EnvFile{}, resp, "TEST Failed.\n")
-}
-
-func TestNewGoDotEnvProvider(t *testing.T) {
-	f := new(EnvFile)
-
-	t.Setenv("APP_NAME", "gofr-sample")
-
-	app := f.Get("APP_NAME")
-
-	assert.Equal(t, "gofr-sample", app, "TEST Failed.\n")
-}
-
-func TestEnvFile_GetOrDefault(t *testing.T) {
-	var (
-		key   = "random123"
-		value = "value123"
-		f     = new(EnvFile)
-	)
-
-	t.Setenv(key, value)
-
-	tests := []struct {
-		desc  string
-		key   string
-		value string
-	}{
-		{"success case", key, value},
-		{"key doesn't exists", "someKeyThatDoesntExist", "default"},
+func Test_EnvSuccess(t *testing.T) {
+	envData := map[string]string{
+		"DATABASE_URL": "localhost:5432",
+		"API_KEY":      "your_api_key_here",
+		"small_case":   "small_case_value",
 	}
 
-	for i, tc := range tests {
-		resp := f.GetOrDefault(tc.key, "default")
-
-		assert.Equal(t, tc.value, resp, "TEST[%d], Failed.\n%s", i, tc.desc)
+	// Call the function to create the .env file
+	err := createEnvFile(t, envData)
+	if err != nil {
+		t.Fatalf("Error creating env file : %v", err)
 	}
+
+	defer os.RemoveAll("configs")
+
+	env := NewEnvFile("configs")
+
+	assert.Equal(t, "localhost:5432", env.Get("DATABASE_URL"))
+	assert.Equal(t, "your_api_key_here", env.GetOrDefault("API_KEY", "xyz"))
+	assert.Equal(t, "test", env.GetOrDefault("DATABASE", "test"))
+	assert.Equal(t, "small_case_value", env.Get("small_case"))
+}
+
+func Test_EnvFailureWithHypen(t *testing.T) {
+	envData := map[string]string{
+		"KEY-WITH-HYPHEN": "DASH-VALUE",
+		"UNABLE_TO_LOAD":  "VALUE",
+	}
+
+	// Call the function to create the .env file
+	err := createEnvFile(t, envData)
+	if err != nil {
+		t.Fatalf("Error creating env file : %v", err)
+	}
+
+	defer os.RemoveAll("configs")
+
+	env := NewEnvFile("configs")
+
+	assert.Equal(t, "test", env.GetOrDefault("KEY-WITH-HYPHEN", "test"))
+	assert.Equal(t, "", env.Get("UNABLE_TO_LOAD"))
+}
+
+func createEnvFile(t *testing.T, envData map[string]string) error {
+	err := os.Mkdir("configs", os.ModePerm)
+	if err != nil {
+		t.Fatalf("unable to create configs directory %v", err)
+	}
+
+	// Create or open the .env file for writing
+	envFile, err := os.Create("configs/.env")
+	if err != nil {
+		t.Fatalf("error creating .env file: %v", err)
+	}
+
+	defer envFile.Close()
+
+	// Write data to the .env file
+	for key, value := range envData {
+		_, err := fmt.Fprintf(envFile, "%s=%s\n", key, value)
+		if err != nil {
+			t.Fatalf("unable to write to file: %v", err)
+		}
+	}
+
+	return nil
 }
