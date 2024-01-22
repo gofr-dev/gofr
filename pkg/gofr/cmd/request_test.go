c779d2d9142a1a119d6a0fcdc7ed95cc3e378e13
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-01-23 01:48:00 +0530

    resolve linters

--- pkg/gofr/cmd/request_test.go
+++ pkg/gofr/cmd/request_test.go
@@ -1,21 +1,22 @@
 package cmd
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRequest_Bind(t *testing.T) {
 	// TODO: Only fields starting with Capital letter can be 'bind' right now.
 	r := NewRequest([]string{"command", "-Name=gofr", "-Valid=true", "-Value=12", "-test", "--name=Gofr"})
 
-	assert.Equal(t, "gofr", r.Param("Name"))
+	assert.Equal(t, "gofr", r.Param("Name"), "TEST Failed.\n Unable to read param from request")
 
-	assert.Equal(t, "true", r.Param("test"))
+	assert.Equal(t, "true", r.Param("test"), "TEST Failed.\n Unable to read param from request")
 
-	assert.Equal(t, "12", r.Param("Value"))
+	assert.Equal(t, "12", r.Param("Value"), "TEST Failed.\n Unable to read param from request")
 
-	assert.Equal(t, "Gofr", r.Param("name"))
+	assert.Equal(t, "Gofr", r.Param("name"), "TEST Failed.\n Unable to read param from request")
 
 	// Testing string, bool, int
 	a := struct {
