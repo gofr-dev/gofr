2ba0a198bbccef98edfd016998ae1f2d7564c1a4
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-01-24 16:46:11 +0530

    add container test

--- pkg/gofr/container/container_test.go
+++ pkg/gofr/container/container_test.go
@@ -1,11 +1,16 @@
 package container
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"gofr.dev/pkg/gofr/config"
+	"gofr.dev/pkg/gofr/service"
+	"gofr.dev/pkg/gofr/testutil"
 )
 
 func Test_newContainerSuccessWithLogger(t *testing.T) {
@@ -47,4 +52,38 @@ func Test_newContainerDBIntializationFail(t *testing.T) {
 	// will not suffice the purpose of this test
 	assert.Nil(t, container.DB, "TEST, Failed.\ninvalid db connections")
 	assert.Nil(t, container.Redis, "TEST, Failed.\ninvalid redis connections")
+}
+
+func Test_HealthCheckServiceStatusUP(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	container := NewContainer(config.NewEnvFile(""))
+
+	container.Services = make(map[string]service.HTTPService)
+
+	container.Services["test-service"] = service.NewHTTPService(server.URL, testutil.NewMockLogger(testutil.INFOLOG))
+
+	response := container.Health(context.Background())
+
+	assert.Equal(t, response, Health{Status: StatusUp, Services: []ServiceHealth{{Name: "test-service", Status: StatusUp}}})
+}
+
+func Test_HealthCheckServiceStatusDegraded(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer server.Close()
+
+	container := NewContainer(config.NewEnvFile(""))
+
+	container.Services = make(map[string]service.HTTPService)
+
+	container.Services["test-service"] = service.NewHTTPService(server.URL, testutil.NewMockLogger(testutil.INFOLOG))
+
+	response := container.Health(context.Background())
+
+	assert.Equal(t, response, Health{Status: StatusDegraded, Services: []ServiceHealth{{Name: "test-service", Status: StatusDown}}})
 }
