726e4a2585382f2bf85844a08140f7bc399aba92
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-01-25 16:01:35 +0530

    refactor sql and redis initialization

--- pkg/gofr/container/default.go
+++
@@ -1,6 +0,0 @@
-package container
-
-const (
-	defaultDBPort    = 3306
-	defaultRedisPort = 6379
-)
--- pkg/gofr/datasource/sql/sql.go
+++ pkg/gofr/datasource/sql/sql.go
@@ -3,9 +3,13 @@ package sql
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
 
+	"gofr.dev/pkg/gofr/config"
 	"gofr.dev/pkg/gofr/datasource"
 )
+
+const defaultDBPort = 3306
 
 // DBConfig has those members which are necessary variables while connecting to database.
 type DBConfig struct {
@@ -16,23 +20,48 @@ type DBConfig struct {
 	Database string
 }
 
-func NewMYSQL(config *DBConfig, logger datasource.Logger) (*DB, error) {
+func NewSQL(configs config.Config, logger datasource.Logger) *DB {
+	dbConfig := getDBConfig(configs)
+
+	// if Hostname is not provided, we won't try to connect to DB
+	if dbConfig.HostName == "" {
+		return nil
+	}
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local&interpolateParams=true",
-		config.User,
-		config.Password,
-		config.HostName,
-		config.Port,
-		config.Database,
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.HostName,
+		dbConfig.Port,
+		dbConfig.Database,
 	)
 
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		return nil, err
+		logger.Errorf("could not connect with '%s' user to database '%s:%s'  error: %v",
+			dbConfig.User, dbConfig.HostName, dbConfig.Port, err)
+
+		return &DB{config: dbConfig}
 	}
 
 	if err := db.Ping(); err != nil {
-		return nil, err
+		logger.Errorf("could not connect with '%s' user to database '%s:%s'  error: %v",
+			dbConfig.User, dbConfig.HostName, dbConfig.Port, err)
+
+		return &DB{config: dbConfig}
 	}
 
-	return &DB{DB: db, logger: logger}, nil
+	logger.Logf("connected to '%s' database at %s:%s", dbConfig.Database, dbConfig.HostName, dbConfig.Port)
+
+	return &DB{DB: db, config: dbConfig, logger: logger}
+}
+
+func getDBConfig(configs config.Config) *DBConfig {
+	return &DBConfig{
+		HostName: configs.Get("DB_HOST"),
+		User:     configs.Get("DB_USER"),
+		Password: configs.Get("DB_PASSWORD"),
+		Port:     configs.GetOrDefault("DB_PORT", strconv.Itoa(defaultDBPort)),
+		Database: configs.Get("DB_NAME"),
+	}
 }
