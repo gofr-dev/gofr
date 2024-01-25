e67b2fedde35283b310d8337f77a216fd8783763
umang01-hash <mundhraumang.02@gmail.com>
2024-01-25 12:04:31 +0530

    remove extra spaces in logging and implement stringer interface on query logging

---
+++ examples/http-server-using-redis/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"sync"
+	"time"
+
+	"gofr.dev/pkg/gofr"
+)
+
+func main() {
+	// Create a new application
+	a := gofr.New()
+
+	a.AddHTTPService("anotherService", "http://localhost:9000")
+
+	// Add all the routes
+	a.GET("/hello", HelloHandler)
+	a.GET("/error", ErrorHandler)
+	a.GET("/redis", RedisHandler)
+	a.GET("/trace", TraceHandler)
+	a.GET("/mysql", MysqlHandler)
+
+	// Run the application
+	a.Run()
+}
+
+func HelloHandler(c *gofr.Context) (interface{}, error) {
+	name := c.Param("name")
+	if name == "" {
+		c.Log("Name came empty")
+		name = "World"
+	}
+
+	return fmt.Sprintf("Hello %s!", name), nil
+}
+
+func ErrorHandler(c *gofr.Context) (interface{}, error) {
+	return nil, errors.New("some error occurred")
+}
+
+func RedisHandler(c *gofr.Context) (interface{}, error) {
+	_, err := c.Redis.HMSet(c, "test2", "value2", "value3", "value4").Result()
+	if err != nil && !errors.Is(err, redis.Nil) { // If key is not found, we are not considering this an error and returning "".
+		return nil, err
+	}
+
+	pipe := c.Redis.Pipeline()
+	pipe.Get(c, "test").Result()
+	pipe.Set(c, "test", "testValue", 1*time.Minute)
+
+	res, err := pipe.Exec(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func TraceHandler(c *gofr.Context) (interface{}, error) {
+	defer c.Trace("traceHandler").End()
+
+	span2 := c.Trace("some-sample-work")
+	<-time.After(time.Millisecond * 1) //nolint:wsl    // Waiting for 1ms to simulate workload
+	span2.End()
+
+	// Ping redis 5 times concurrently and wait.
+	count := 5
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+
+	for i := 0; i < count; i++ {
+		go func() {
+			c.Redis.Ping(c)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	//Call Another service
+	resp, err := c.GetHTTPService("anotherService").Get(c, "redis", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func MysqlHandler(c *gofr.Context) (interface{}, error) {
+	var value int
+	err := c.DB.QueryRowContext(c, "select 2+2").Scan(&value)
+
+	return value, err
+}
---
+++ examples/http-server-using-redis/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleAPIServer(t *testing.T) {
+	const host = "http://localhost:9000"
+	go main()
+	time.Sleep(time.Second * 3) // Giving some time to start the server
+
+	tests := []struct {
+		desc       string
+		path       string
+		statusCode int
+	}{
+		{"empty path", "/", 404},
+		{"hello handler", "/hello", 200},
+		{"hello handler with query parameter", "/hello?name=gofr", 200},
+		{"error handler", "/error", 500},
+		{"redis handler", "/redis", 200},
+		{"trace handler", "/trace", 200},
+		{"mysql handler", "/mysql", 200},
+		{"health handler", "/.well-known/health", 200}, // Health check should be added by the framework.
+		{"favicon handler", "/favicon.ico", 200},       //Favicon should be added by the framework.
+	}
+
+	for i, tc := range tests {
+		req, _ := http.NewRequest("GET", host+tc.path, nil)
+		c := http.Client{}
+		resp, err := c.Do(req)
+
+		assert.Nil(t, err, "TEST[%d], Failed.\n%s", i, tc.desc)
+
+		assert.Equal(t, tc.statusCode, resp.StatusCode, "TEST[%d], Failed.\n%s", i, tc.desc)
+	}
+}
