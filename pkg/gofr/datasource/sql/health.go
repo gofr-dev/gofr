ae36dd543a985b345b69a960b63ea73620163e3c
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-01-25 16:18:29 +0530

    use custom DB stats structure

--- pkg/gofr/datasource/sql/health.go
+++ pkg/gofr/datasource/sql/health.go
@@ -7,6 +7,22 @@ import (
 	"gofr.dev/pkg/gofr/datasource"
 )
 
+type DBStats struct {
+	MaxOpenConnections int `json:"maxOpenConnections"` // Maximum number of open connections to the database.
+
+	// Pool Status
+	OpenConnections int `json:"openConnections"` // The number of established connections both in use and idle.
+	InUse           int `json:"inUse"`           // The number of connections currently in use.
+	Idle            int `json:"idle"`            // The number of idle connections.
+
+	// Counters
+	WaitCount         int64         `json:"waitCount"`         // The total number of connections waited for.
+	WaitDuration      time.Duration `json:"waitDuration"`      // The total time blocked waiting for a new connection.
+	MaxIdleClosed     int64         `json:"maxIdleClosed"`     // The total number of connections closed due to SetMaxIdleConns.
+	MaxIdleTimeClosed int64         `json:"maxIdleTimeClosed"` // The total number of connections closed due to SetConnMaxIdleTime.
+	MaxLifetimeClosed int64         `json:"maxLifetimeClosed"` // The total number of connections closed due to SetConnMaxLifetime.
+}
+
 func (d *DB) HealthCheck() *datasource.Health {
 	h := datasource.Health{
 		Details: make(map[string]interface{}),
@@ -31,7 +47,19 @@ func (d *DB) HealthCheck() *datasource.Health {
 	}
 
 	h.Status = datasource.StatusUp
-	h.Details["stats"] = d.Stats()
+
+	dbStats := d.Stats()
+	h.Details["stats"] = DBStats{
+		MaxOpenConnections: dbStats.MaxOpenConnections,
+		OpenConnections:    dbStats.OpenConnections,
+		InUse:              dbStats.InUse,
+		Idle:               dbStats.Idle,
+		WaitCount:          dbStats.WaitCount,
+		WaitDuration:       dbStats.WaitDuration,
+		MaxIdleClosed:      dbStats.MaxIdleClosed,
+		MaxIdleTimeClosed:  dbStats.MaxIdleTimeClosed,
+		MaxLifetimeClosed:  dbStats.MaxLifetimeClosed,
+	}
 
 	return &h
 }
