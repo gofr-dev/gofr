829293d7dd2ae242d1a8a3f1ade09b2acba49bc9
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-02-05 15:18:06 +0530

    add metrics

--- pkg/gofr/container/container.go
+++ pkg/gofr/container/container.go
@@ -1,10 +1,14 @@
 package container
 
 import (
+	"go.opentelemetry.io/otel/metric"
+
 	"gofr.dev/pkg/gofr/config"
 	"gofr.dev/pkg/gofr/datasource/redis"
 	"gofr.dev/pkg/gofr/datasource/sql"
 	"gofr.dev/pkg/gofr/logging"
+	"gofr.dev/pkg/gofr/metrics"
+	"gofr.dev/pkg/gofr/metrics/exporters"
 	"gofr.dev/pkg/gofr/service"
 
 	_ "github.com/go-sql-driver/mysql" // This is required to be blank import
@@ -16,9 +20,13 @@ import (
 // etc which is shared across is placed here.
 type Container struct {
 	logging.Logger
-	Services map[string]service.HTTP
-	Redis    *redis.Redis
-	DB       *sql.DB
+
+	Services       map[string]service.HTTP
+	MetricsManager metrics.Manager
+	exporter       metric.Meter
+
+	Redis *redis.Redis
+	DB    *sql.DB
 }
 
 func NewContainer(conf config.Config) *Container {
@@ -32,11 +40,27 @@ func NewContainer(conf config.Config) *Container {
 
 	c.DB = sql.NewSQL(conf, c.Logger)
 
+	if c.exporter == nil {
+		c.exporter = exporters.OTLPStdOut(
+			conf.GetOrDefault("APP_NAME", "gofr-app"),
+			conf.GetOrDefault("APP_VERSION", "dev"))
+	}
+
+	c.MetricsManager = metrics.NewMetricManager(c.exporter)
+
 	return c
+}
+
+func (c *Container) SetMetricsExporter(m metric.Meter) {
+	c.exporter = m
 }
 
 // GetHTTPService returns registered http services.
 // HTTP services are registered from AddHTTPService method of gofr object.
 func (c *Container) GetHTTPService(serviceName string) service.HTTP {
 	return c.Services[serviceName]
+}
+
+func (c *Container) UpdateMetric() metrics.Updater {
+	return c.MetricsManager
 }
---
+++ pkg/gofr/metricsServer.go
@@ -0,0 +1,34 @@
+package gofr
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"gofr.dev/pkg/gofr/container"
+	"gofr.dev/pkg/gofr/metrics"
+)
+
+const defaultMetricPort = "2121"
+
+type metricServer struct {
+	port int
+}
+
+func newMetricServer(port int) *metricServer {
+	return &metricServer{port: port}
+}
+
+func (m *metricServer) Run(c *container.Container) {
+	var srv *http.Server
+
+	c.Logf("Starting metrics server on port: %d", m.port)
+
+	srv = &http.Server{
+		Addr:              fmt.Sprintf(":%d", m.port),
+		Handler:           metrics.GetHandler(),
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	c.Error(srv.ListenAndServe())
+}
