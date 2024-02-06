6e7d16b1b62fc5862add2151204651e22f0e1c15
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-02-06 12:41:48 +0530

    comment unsed exporters

--- pkg/gofr/metrics/exporters/exporter.go
+++ pkg/gofr/metrics/exporters/exporter.go
@@ -1,11 +1,7 @@
 package exporters
 
 import (
-	"time"
-
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/prometheus"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/metric"
 	metricSdk "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -31,31 +27,31 @@ func Prometheus(appName, appVersion string) metric.Meter {
 // TODO : OTLPStdOut and OTLPMetricHTTP are not being used but has to be metrics
 // has to be modified such that user can decide the exporter.
 
-func OTLPStdOut(appName, appVersion string) metric.Meter {
-	exporter, err := stdoutmetric.New()
-	if err != nil {
-		return nil
-	}
-
-	meter := metricSdk.NewMeterProvider(
-		metricSdk.WithResource(resource.NewSchemaless(semconv.ServiceName(appName))),
-		metricSdk.WithReader(metricSdk.NewPeriodicReader(exporter,
-			metricSdk.WithInterval(3*time.Second)))).Meter(appName, metric.WithInstrumentationVersion(appVersion))
-
-	return meter
-}
-
-func OTLPMetricHTTP(appName, appVersion string) metric.Meter {
-	exporter, err := otlpmetrichttp.New(nil,
-		otlpmetrichttp.WithInsecure(),
-		otlpmetrichttp.WithURLPath("/metrics"),
-		otlpmetrichttp.WithEndpoint("localhost:8000"))
-	if err != nil {
-		return nil
-	}
-
-	meter := metricSdk.NewMeterProvider(metricSdk.WithReader(metricSdk.NewPeriodicReader(exporter,
-		metricSdk.WithInterval(3*time.Second)))).Meter(appName, metric.WithInstrumentationVersion(appVersion))
-
-	return meter
-}
+// func OTLPStdOut(appName, appVersion string) metric.Meter {
+//	exporter, err := stdoutmetric.New()
+//	if err != nil {
+//		return nil
+//	}
+//
+//	meter := metricSdk.NewMeterProvider(
+//		metricSdk.WithResource(resource.NewSchemaless(semconv.ServiceName(appName))),
+//		metricSdk.WithReader(metricSdk.NewPeriodicReader(exporter,
+//			metricSdk.WithInterval(3*time.Second)))).Meter(appName, metric.WithInstrumentationVersion(appVersion))
+//
+//	return meter
+//}
+//
+// func OTLPMetricHTTP(appName, appVersion string) metric.Meter {
+//	exporter, err := otlpmetrichttp.New(nil,
+//		otlpmetrichttp.WithInsecure(),
+//		otlpmetrichttp.WithURLPath("/metrics"),
+//		otlpmetrichttp.WithEndpoint("localhost:8000"))
+//	if err != nil {
+//		return nil
+//	}
+//
+//	meter := metricSdk.NewMeterProvider(metricSdk.WithReader(metricSdk.NewPeriodicReader(exporter,
+//		metricSdk.WithInterval(3*time.Second)))).Meter(appName, metric.WithInstrumentationVersion(appVersion))
+//
+//	return meter
+//}
