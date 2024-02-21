a8270fb065f90031d6274927c52d24dfba2f0e56
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2024-02-21 22:18:33 +0530

    increase test coverage

--- pkg/gofr/service/new_test.go
+++ pkg/gofr/service/new_test.go
@@ -33,6 +33,10 @@ func TestNewHTTPService(t *testing.T) {
 func TestHTTPService_Get(t *testing.T) {
 	// Setup a test server
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+
 		w.WriteHeader(http.StatusOK)
 	}))
 	defer server.Close()
@@ -44,28 +48,364 @@ func TestHTTPService_Get(t *testing.T) {
 		Logger: testutil.NewMockLogger(testutil.INFOLOG),
 	}
 
-	tests := []struct {
-		desc   string
-		path   string
-		params map[string]interface{}
-		status int
-	}{
-		{"Valid Request", "path", map[string]interface{}{"key": "value"}, http.StatusOK},
-		{"Request with Empty Path", "", map[string]interface{}{"key": "value"}, http.StatusOK},
-		{"Request With Params", "path", map[string]interface{}{"name": []string{"gofr"}}, http.StatusOK},
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.Get(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+
+}
+
+func TestHTTPService_GetWithHeaders(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "value1", r.Header.Get("header1"))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
 	}
 
-	for i, tc := range tests {
-		t.Run(tc.desc, func(t *testing.T) {
-			ctx := context.Background()
-			resp, err := service.Get(ctx, tc.path, tc.params)
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
 
-			assert.NoError(t, err)
-			assert.NotNil(t, resp, "TEST[%d], Failed.\n%s", i, tc.desc)
+	resp, err := service.GetWithHeaders(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}},
+		map[string]string{"header1": "value1"})
 
-			defer resp.Body.Close()
-		})
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+
+}
+
+func TestHTTPService_Put(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodPut, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
 	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.Put(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}}, []byte("{Test Body}"))
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_PutWithHeaders(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodPut, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "value1", r.Header.Get("header1"))
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.PutWithHeaders(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}}, []byte("{Test Body}"),
+		map[string]string{"header1": "value1"})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_Patch(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodPatch, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.Patch(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}}, []byte("{Test Body}"))
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_PatchWithHeaders(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodPut, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "value1", r.Header.Get("header1"))
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.PutWithHeaders(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}}, []byte("{Test Body}"),
+		map[string]string{"header1": "value1"})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_Post(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.Post(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}}, []byte("{Test Body}"))
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_PostWithHeaders(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Equal(t, "key=value&name=test", r.URL.RawQuery)
+		assert.Contains(t, "value1", r.Header.Get("header1"))
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.PostWithHeaders(context.Background(), "test-path",
+		map[string]interface{}{"key": "value", "name": []string{"gofr", "test"}}, []byte("{Test Body}"),
+		map[string]string{"header1": "value1"})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_Delete(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodDelete, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.Delete(context.Background(), "test-path", []byte("{Test Body}"))
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
+}
+
+func TestHTTPService_DeleteWithHeaders(t *testing.T) {
+	// Setup a test server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// read request body
+		var body []byte
+
+		_, err := r.Body.Read(body)
+		if err != nil {
+			t.Fatal("Unable to read request body")
+		}
+
+		assert.Equal(t, http.MethodDelete, r.Method)
+		assert.Equal(t, "/test-path", r.URL.Path)
+		assert.Contains(t, "value1", r.Header.Get("header1"))
+		assert.Contains(t, "Test Body", string(body))
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	service := &httpService{
+		Client: http.DefaultClient,
+		url:    server.URL,
+		Tracer: otel.Tracer("gofr-http-client"),
+		Logger: testutil.NewMockLogger(testutil.INFOLOG),
+	}
+
+	// TODO : Nil Correlation ID is coming in logs, it has to be fixed
+
+	resp, err := service.DeleteWithHeaders(context.Background(), "test-path", []byte("{Test Body}"),
+		map[string]string{"header1": "value1"})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp, "TEST, Failed.")
+
+	defer resp.Body.Close()
 }
 
 func TestHTTPService_createAndSendRequest(t *testing.T) {
