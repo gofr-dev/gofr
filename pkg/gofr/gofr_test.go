3816fc622b17eca86c5fc29c7ec68712ac6ae36e
Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>
2024-02-23 11:17:55 +0530

    Increase test coverage (#290)
    * add failure case for redis health-check
    
    * add tests for testutil
    
    * add and refactor tests
    
    * resolve merge conflicts

--- pkg/gofr/gofr_test.go
+++ pkg/gofr/gofr_test.go
@@ -130,3 +130,22 @@ func TestGofr_ServerRun(t *testing.T) {
 
 	resp.Body.Close()
 }
+
+func Test_AddHTTPService(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/test", r.URL.Path)
+
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	g := New()
+
+	g.AddHTTPService("test-service", server.URL)
+
+	resp, _ := g.container.GetHTTPService("test-service").
+		Get(context.Background(), "test", nil)
+
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
--- pkg/gofr/service/metrics.go
+++ pkg/gofr/service/metrics.go
@@ -3,8 +3,5 @@ package service
 import "context"
 
 type Metrics interface {
-	IncrementCounter(ctx context.Context, name string, labels ...string)
-	DeltaUpDownCounter(ctx context.Context, name string, value float64, labels ...string)
 	RecordHistogram(ctx context.Context, name string, value float64, labels ...string)
-	SetGauge(name string, value float64)
 }
