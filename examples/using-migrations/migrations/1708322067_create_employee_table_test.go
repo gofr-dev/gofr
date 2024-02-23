fd5b9aabedf218264f2282f03b7d2bc0bda24a7f
Umang Mundhra <mundhraumang.02@gmail.com>
2024-02-23 18:56:23 +0530

    Add tests for migrations example (#296)

---
+++ examples/using-migrations/migrations/1708322067_create_employee_table_test.go
@@ -0,0 +1,71 @@
+package migrations
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"gofr.dev/pkg/gofr/migration"
+)
+
+func TestCreateTableEmployee(t *testing.T) {
+	tests := []struct {
+		name          string
+		mockBehaviors func(mock sqlmock.Sqlmock)
+		expectedError error
+	}{
+		{
+			name: "SuccessfulExecution",
+			mockBehaviors: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(createTable).WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec(employee_date).WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec("alter table employee add dob varchar(11) null;").WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			expectedError: nil,
+		},
+		{
+			name: "ErrorOnCreateTable",
+			mockBehaviors: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(createTable).WillReturnError(fmt.Errorf("create table error"))
+			},
+			expectedError: fmt.Errorf("create table error"),
+		},
+		{
+			name: "ErrorOnInsert",
+			mockBehaviors: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(createTable).WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec(employee_date).WillReturnError(fmt.Errorf("insert error"))
+			},
+			expectedError: fmt.Errorf("insert error"),
+		},
+		{
+			name: "ErrorOnAlterTable",
+			mockBehaviors: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(createTable).WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec(employee_date).WillReturnResult(sqlmock.NewResult(0, 1))
+				mock.ExpectExec("alter table employee add dob varchar(11) null;").WillReturnError(fmt.Errorf("alter table error"))
+			},
+			expectedError: fmt.Errorf("alter table error"),
+		},
+	}
+
+	for i, tc := range tests {
+		// Create mock database and datasource
+		db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		assert.NoError(t, err)
+		defer db.Close()
+
+		datasource := migration.Datasource{DB: db}
+
+		// Set mock expectations
+		tc.mockBehaviors(mock)
+
+		// Execute the migration
+		err = createTableEmployee().UP(datasource)
+
+		assert.Equal(t, err, tc.expectedError, "TEST[%d] failed! Desc : %v", i, tc.name)
+
+		assert.NoError(t, mock.ExpectationsWereMet())
+	}
+}
--- go.mod
+++ go.mod
@@ -46,6 +46,7 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-redis/redismock/v9 v9.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
