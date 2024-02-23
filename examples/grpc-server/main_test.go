55dc1193503cb98551132d741ef1d25669d5cf4a
Umang Mundhra <mundhraumang.02@gmail.com>
2024-02-23 13:32:47 +0530

    En/grpc example test (#292)
    * initial commit
    
    * initial commit
    
    * resolve port conflict
    
    * remove unwanted file
    
    * include static folder
    
    * add favicon file

--- examples/grpc-server/main_test.go
+++ examples/grpc-server/main_test.go
@@ -1,7 +1,76 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	grpcExample "gofr.dev/examples/grpc-server/grpc"
+)
 
 func TestGRPCServer(t *testing.T) {
+	const host = "localhost:10000"
 
+	go main()
+	time.Sleep(time.Second * 1)
+
+	client, conn := createGRPCClient(t, host)
+	defer conn.Close()
+
+	tests := []struct {
+		desc            string
+		request         *grpcExample.HelloRequest
+		expectedErr     error
+		responseMessage string
+	}{
+		{"SayHello with name", &grpcExample.HelloRequest{Name: "John"},
+			nil, "Hello John!"},
+		{"SayHello without name", &grpcExample.HelloRequest{},
+			nil, "Hello World!"},
+		{"Name exceeding limit", &grpcExample.HelloRequest{Name: "This name exceeds the allowed maximum length."},
+			nil, "Hello This name exceeds the allowed maximum length.!"},
+	}
+
+	for _, tc := range tests {
+		resp, err := client.SayHello(context.Background(), tc.request)
+		assert.NoError(t, err)
+		assert.Equal(t, tc.responseMessage, resp.Message)
+	}
+
+	// case of empty request
+	_, err := client.SayHello(context.Background(), nil)
+	assert.Equal(t, "rpc error: code = Internal desc = grpc: error while marshaling: proto: "+
+		"Marshal called with nil", err.Error())
+
+	// Test context cancellation
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	_, err = client.SayHello(ctx, &grpcExample.HelloRequest{Name: "Test"})
+	assert.Equal(t, err.Error(), "rpc error: code = Canceled desc = context canceled")
+
+}
+
+func createGRPCClient(t *testing.T, host string) (grpcExample.HelloClient, *grpc.ClientConn) {
+	conn, err := grpc.Dial(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Errorf("did not connect: %s", err)
+	}
+
+	return grpcExample.NewHelloClient(conn), conn
+}
+
+func TestHelloProtoMethods(t *testing.T) {
+	// Test HelloRequest methods
+	req := &grpcExample.HelloRequest{Name: "John"}
+	assert.Equal(t, "John", req.GetName())
+	assert.Equal(t, "name:\"John\"", req.String())
+
+	// Test HelloResponse methods
+	resp := &grpcExample.HelloResponse{Message: "Hello World"}
+	assert.Equal(t, "Hello World", resp.GetMessage())
+	assert.Equal(t, "message:\"Hello World\"", resp.String())
 }
