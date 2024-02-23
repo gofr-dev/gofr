035cb2ef45a218e15be1e3b8fcae3130cb889352
Vipul Rawat <vipul.rawat@zopsmart.com>
2024-02-23 17:00:25 +0530

    Add Publisher/Subscriber support for Google PubSub and Apache Kafka (#283)
    * skeleton code
    
    * add Client interface and give control to decide client type to pubsub
    
    * initial kafka implementation
    
    * implementation with AutoCommit
    
    * minor refactorations
    
    * add google pubsub and add switch case to populate in container
    
    * add offset in kafka pubsub
    
    * undo unwanted changes
    
    * add a check if subscription for pubsub is registered
    
    * add Committer functionality to commit messages and refactoring code
    
    * remove unnecessary files
    
    * remove unnecessary envs
    
    * refactor examples
    
    * fixed linter issues
    
    * refactoring google PubSub's subscribe
    
    * remove non-necessary error message global
    
    * remove msg ack from subscribe function
    
    * add google test cases for publishing message and helper functions
    
    * add tests for pubsub message functionality
    
    * remove Topic config for kafka publisher subscriber
    
    * add config validator for google and kafka
    
    * refactored message for google and kafka clients
    
    * add publish time in messages
    
    ---------
    
    Co-authored-by: umang01-hash <mundhraumang.02@gmail.com>

---
+++ examples/using-publisher/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+
+	"gofr.dev/pkg/gofr"
+)
+
+func main() {
+	app := gofr.New()
+
+	app.GET("/publish-order", order)
+
+	app.GET("/publish-product", product)
+
+	app.Run()
+}
+
+func order(ctx *gofr.Context) (interface{}, error) {
+	type orderStatus struct {
+		OrderId string `json:"orderId"`
+		Status  string `json:"status"`
+	}
+
+	var data orderStatus
+
+	err := ctx.Bind(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	msg, _ := json.Marshal(data)
+
+	err = ctx.GetPublisher().Publish(ctx, "order-logs", msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return "Published", nil
+}
+
+func product(ctx *gofr.Context) (interface{}, error) {
+	type productInfo struct {
+		ProductId string `json:"productId"`
+		Price     string `json:"price"`
+	}
+
+	var data productInfo
+
+	err := ctx.Bind(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	msg, _ := json.Marshal(data)
+
+	err = ctx.GetPublisher().Publish(ctx, "products", msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return "Published", nil
+}
---
+++ pkg/gofr/datasource/pubsub/google/google_test.go
@@ -0,0 +1,151 @@
+package google
+
+import (
+	"context"
+	"testing"
+
+	gcPubSub "cloud.google.com/go/pubsub"
+	"cloud.google.com/go/pubsub/pstest"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"gofr.dev/pkg/gofr/testutil"
+)
+
+func getGoogleClient(t *testing.T) *gcPubSub.Client {
+	srv := pstest.NewServer()
+
+	conn, err := grpc.Dial(srv.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Errorf("could not initialize a connection to dummy server")
+	}
+
+	client, err := gcPubSub.NewClient(context.Background(), "project", option.WithGRPCConn(conn))
+	if err != nil {
+		t.Errorf("could not initialize a test client")
+	}
+
+	return client
+}
+
+func TestGoogleClient_New_Error(t *testing.T) {
+	var (
+		g *googleClient
+	)
+
+	out := testutil.StderrOutputForFunc(func() {
+		logger := testutil.NewMockLogger(testutil.ERRORLOG)
+
+		g = New(Config{}, logger)
+	})
+
+	assert.Nil(t, g)
+	assert.Contains(t, out, "google pubsub could not be configured")
+}
+
+func TestGoogleClient_Publish_Success(t *testing.T) {
+	client := getGoogleClient(t)
+	defer client.Close()
+
+	topic := "test-topic"
+	message := []byte("test message")
+	expectedLog := "published google message test message on topic test-topic\n"
+
+	out := testutil.StdoutOutputForFunc(func() {
+		g := &googleClient{
+			logger: testutil.NewMockLogger(testutil.DEBUGLOG),
+			client: client,
+			Config: Config{
+				ProjectID:        "test",
+				SubscriptionName: "sub",
+			},
+		}
+
+		err := g.Publish(context.Background(), topic, message)
+
+		assert.Nil(t, err)
+	})
+
+	assert.Equal(t, expectedLog, out)
+}
+
+func TestGoogleClient_PublishTopic_Error(t *testing.T) {
+	g := &googleClient{client: getGoogleClient(t), Config: Config{
+		ProjectID:        "test",
+		SubscriptionName: "sub",
+	}}
+	defer g.client.Close()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cancel()
+
+	err := g.Publish(ctx, "test-topic", []byte(""))
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "context canceled")
+	}
+}
+
+func TestGoogleClient_getTopic_Success(t *testing.T) {
+	g := &googleClient{client: getGoogleClient(t), Config: Config{
+		ProjectID:        "test",
+		SubscriptionName: "sub",
+	}}
+	defer g.client.Close()
+
+	topic, err := g.getTopic(context.Background(), "test-topic")
+
+	assert.Nil(t, err)
+	assert.Equal(t, topic.ID(), "test-topic")
+}
+
+func TestGoogleClient_getTopic_Error(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	g := &googleClient{client: getGoogleClient(t), Config: Config{
+		ProjectID:        "test",
+		SubscriptionName: "sub",
+	}}
+	defer g.client.Close()
+
+	topic, err := g.getTopic(ctx, "test-topic")
+
+	assert.Nil(t, topic)
+	assert.Contains(t, err.Error(), "context canceled")
+}
+
+func TestGoogleClient_getSubscription(t *testing.T) {
+	g := &googleClient{client: getGoogleClient(t), Config: Config{
+		ProjectID:        "test",
+		SubscriptionName: "sub",
+	}}
+	defer g.client.Close()
+
+	topic, _ := g.client.CreateTopic(context.Background(), "test-topic")
+
+	sub, err := g.getSubscription(context.Background(), topic)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sub)
+}
+
+func Test_validateConfigs(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		input  *Config
+		expErr error
+	}{
+		{desc: "project id not provided", input: &Config{}, expErr: errProjectIDNotProvided},
+		{desc: "subscription not provided", input: &Config{ProjectID: "test"}, expErr: errSubscriptionNotProvided},
+		{desc: "success", input: &Config{ProjectID: "test", SubscriptionName: "subs"}, expErr: nil},
+	}
+
+	for _, tc := range testCases {
+		err := validateConfigs(tc.input)
+
+		assert.Equal(t, tc.expErr, err)
+	}
+}
