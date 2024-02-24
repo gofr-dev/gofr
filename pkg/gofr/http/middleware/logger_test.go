1f435331f64f57b65fd78c1029e8a1650636662d
Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>
2024-02-24 14:45:30 +0530

    Add Logger Test (#301)
    * add logger test

--- pkg/gofr/http/middleware/logger_test.go
+++ pkg/gofr/http/middleware/logger_test.go
@@ -3,9 +3,12 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"gofr.dev/pkg/gofr/testutil"
 )
 
 func Test_getIPAddress(t *testing.T) {
@@ -34,4 +37,81 @@ func Test_getIPAddress(t *testing.T) {
 
 		assert.Equal(t, addr, ip, "TEST Failed.\n")
 	}
+}
+
+func Test_LoggingMiddleware(t *testing.T) {
+	logs := testutil.StdoutOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "GET    200")
+}
+
+// Test handler that uses the middleware.
+func testHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Test Handler"))
+}
+
+func Test_LoggingMiddlewareStringPanicHandling(t *testing.T) {
+	logs := testutil.StderrOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testStringPanicHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testStringPanicHandler")
+}
+
+// Test handler that uses the middleware.
+func testStringPanicHandler(_ http.ResponseWriter, r *http.Request) {
+	panic(r.URL.Path)
+}
+
+func Test_LoggingMiddlewareErrorPanicHandling(t *testing.T) {
+	logs := testutil.StderrOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testErrorPanicHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testErrorPanicHandler")
+}
+
+// Test handler that uses the middleware.
+func testErrorPanicHandler(http.ResponseWriter, *http.Request) {
+	panic(testutil.CustomError{ErrorMessage: "panic"})
+}
+
+func Test_LoggingMiddlewareUnknownPanicHandling(t *testing.T) {
+	logs := testutil.StderrOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testUnknownPanicHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testUnknownPanicHandler")
+}
+
+// Test handler that uses the middleware.
+func testUnknownPanicHandler(w http.ResponseWriter, _ *http.Request) {
+	panic(w)
 }
