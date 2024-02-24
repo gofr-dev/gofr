edd3ef745eea2790db85370add6db9c5a67a66bf
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-02-24 14:42:25 +0530

    resolve linters

--- pkg/gofr/http/middleware/logger_test.go
+++ pkg/gofr/http/middleware/logger_test.go
@@ -2,12 +2,13 @@ package middleware
 
 import (
 	"context"
-	"gofr.dev/pkg/gofr/testutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"gofr.dev/pkg/gofr/testutil"
 )
 
 func Test_getIPAddress(t *testing.T) {
@@ -52,10 +53,10 @@ func Test_LoggingMiddleware(t *testing.T) {
 	assert.Contains(t, logs, "GET    200")
 }
 
-// Test handler that uses the middleware
-func testHandler(w http.ResponseWriter, r *http.Request) {
+// Test handler that uses the middleware.
+func testHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Test Handler"))
+	_, _ = w.Write([]byte("Test Handler"))
 }
 
 func Test_LoggingMiddlewareStringPanicHandling(t *testing.T) {
@@ -72,12 +73,9 @@ func Test_LoggingMiddlewareStringPanicHandling(t *testing.T) {
 	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testStringPanicHandler")
 }
 
-// Test handler that uses the middleware
-func testStringPanicHandler(w http.ResponseWriter, r *http.Request) {
+// Test handler that uses the middleware.
+func testStringPanicHandler(_ http.ResponseWriter, r *http.Request) {
 	panic(r.URL.Path)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Test Handler"))
 }
 
 func Test_LoggingMiddlewareErrorPanicHandling(t *testing.T) {
@@ -94,12 +92,9 @@ func Test_LoggingMiddlewareErrorPanicHandling(t *testing.T) {
 	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testErrorPanicHandler")
 }
 
-// Test handler that uses the middleware
-func testErrorPanicHandler(w http.ResponseWriter, r *http.Request) {
+// Test handler that uses the middleware.
+func testErrorPanicHandler(http.ResponseWriter, *http.Request) {
 	panic(testutil.CustomError{ErrorMessage: "panic"})
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Test Handler"))
 }
 
 func Test_LoggingMiddlewareUnknownPanicHandling(t *testing.T) {
@@ -116,10 +111,7 @@ func Test_LoggingMiddlewareUnknownPanicHandling(t *testing.T) {
 	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testUnknownPanicHandler")
 }
 
-// Test handler that uses the middleware
-func testUnknownPanicHandler(w http.ResponseWriter, r *http.Request) {
+// Test handler that uses the middleware.
+func testUnknownPanicHandler(w http.ResponseWriter, _ *http.Request) {
 	panic(w)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Test Handler"))
 }
