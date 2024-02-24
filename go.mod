007ba2eb3d5abc8f166d0bcbf3b110ea3e1e0f4b
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-02-24 14:13:02 +0530

    add logger test

--- go.mod
+++ go.mod
@@ -20,13 +20,13 @@ require (
 	github.com/stretchr/testify v1.8.4
 	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.48.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.48.0
-	go.opentelemetry.io/otel v1.23.1
+	go.opentelemetry.io/otel v1.24.0
 	go.opentelemetry.io/otel/exporters/prometheus v0.45.2
 	go.opentelemetry.io/otel/exporters/zipkin v1.23.1
-	go.opentelemetry.io/otel/metric v1.23.1
-	go.opentelemetry.io/otel/sdk v1.23.1
-	go.opentelemetry.io/otel/sdk/metric v1.23.1
-	go.opentelemetry.io/otel/trace v1.23.1
+	go.opentelemetry.io/otel/metric v1.24.0
+	go.opentelemetry.io/otel/sdk v1.24.0
+	go.opentelemetry.io/otel/sdk/metric v1.24.0
+	go.opentelemetry.io/otel/trace v1.24.0
 	go.uber.org/mock v0.4.0
 	golang.org/x/term v0.17.0
 	google.golang.org/api v0.160.0
@@ -47,7 +47,6 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-redis/redismock/v9 v9.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
--- pkg/gofr/http/middleware/logger_test.go
+++ pkg/gofr/http/middleware/logger_test.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"context"
+	"gofr.dev/pkg/gofr/testutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,4 +36,90 @@ func Test_getIPAddress(t *testing.T) {
 
 		assert.Equal(t, addr, ip, "TEST Failed.\n")
 	}
+}
+
+func Test_LoggingMiddleware(t *testing.T) {
+	logs := testutil.StdoutOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "GET    200")
+}
+
+// Test handler that uses the middleware
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Test Handler"))
+}
+
+func Test_LoggingMiddlewareStringPanicHandling(t *testing.T) {
+	logs := testutil.StderrOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testStringPanicHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testStringPanicHandler")
+}
+
+// Test handler that uses the middleware
+func testStringPanicHandler(w http.ResponseWriter, r *http.Request) {
+	panic(r.URL.Path)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Test Handler"))
+}
+
+func Test_LoggingMiddlewareErrorPanicHandling(t *testing.T) {
+	logs := testutil.StderrOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testErrorPanicHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testErrorPanicHandler")
+}
+
+// Test handler that uses the middleware
+func testErrorPanicHandler(w http.ResponseWriter, r *http.Request) {
+	panic(testutil.CustomError{ErrorMessage: "panic"})
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Test Handler"))
+}
+
+func Test_LoggingMiddlewareUnknownPanicHandling(t *testing.T) {
+	logs := testutil.StderrOutputForFunc(func() {
+		req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://dummy", http.NoBody)
+
+		rr := httptest.NewRecorder()
+
+		handler := Logging(testutil.NewMockLogger(testutil.DEBUGLOG))(http.HandlerFunc(testUnknownPanicHandler))
+
+		handler.ServeHTTP(rr, req)
+	})
+
+	assert.Contains(t, logs, "gofr.dev/pkg/gofr/http/middleware.testUnknownPanicHandler")
+}
+
+// Test handler that uses the middleware
+func testUnknownPanicHandler(w http.ResponseWriter, r *http.Request) {
+	panic(w)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Test Handler"))
 }
