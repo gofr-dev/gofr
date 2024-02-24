5aee1d20090cfb62df0f94deb37ea33b8d696226
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-02-24 20:18:59 +0530

    increase test coverage

---
+++ pkg/gofr/datasource/pubsub/kafka/kafka_test.go
@@ -0,0 +1,30 @@
+package kafka
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_validateConfigs(t *testing.T) {
+	config := Config{Broker: "kafkabroker", ConsumerGroupID: "1"}
+
+	err := validateConfigs(config)
+
+	assert.Nil(t, err)
+}
+
+func Test_validateConfigsErrConsumerGroupNotFound(t *testing.T) {
+	config := Config{Broker: "kafkabroker"}
+
+	err := validateConfigs(config)
+
+	assert.Equal(t, errConsumerGroupNotProvided, err)
+}
+
+func Test_validateConfigsErrBrokerNotProvided(t *testing.T) {
+	config := Config{ConsumerGroupID: "1"}
+
+	err := validateConfigs(config)
+
+	assert.Nil(t, err, errBrokerNotProvided)
+}
