a743b4264e8244718ce13497168edf99fb17da13
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-02-24 16:14:47 +0530

    add test for response

---
+++ pkg/gofr/service/response_test.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestResponse_GetHeader(t *testing.T) {
+	// Arrange
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+	response := &Response{
+		headers: headers,
+	}
+
+	result := response.GetHeader("Content-Type")
+	headerNotFound := response.GetHeader("key")
+
+	assert.Equal(t, "application/json", result)
+	assert.Equal(t, "", headerNotFound)
+}
+
+func TestResponse_GetHeaderNil(t *testing.T) {
+	// Arrange
+	response := &Response{}
+
+	result := response.GetHeader("Content-Type")
+
+	assert.Equal(t, "", resul)
+}
