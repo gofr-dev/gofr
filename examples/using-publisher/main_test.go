0dfeb60e354cbd88fe98962a26423c0f5e8aa4bf
Umang Mundhra <mundhraumang.02@gmail.com>
2024-03-01 13:16:00 +0530

    Add integration test for publish failure (#342)

--- examples/using-publisher/main_test.go
+++ examples/using-publisher/main_test.go
@@ -60,3 +60,45 @@ func TestExamplePublisher(t *testing.T) {
 
 	}
 }
+
+func TestExamplePublisherError(t *testing.T) {
+	t.Setenv("PUBSUB_BROKER", "localhost:1012")
+	t.Setenv("HTTP_PORT", "8200")
+
+	const host = "http://localhost:8200"
+	go main()
+	time.Sleep(time.Second * 1)
+
+	testCases := []struct {
+		desc               string
+		path               string
+		body               []byte
+		expectedStatusCode int
+		expectedError      error
+	}{
+		{
+			desc:               "valid order",
+			path:               "/publish-order",
+			body:               []byte(`{"data":{"orderId":"123","status":"pending"}}`),
+			expectedStatusCode: http.StatusInternalServerError,
+		},
+		{
+			desc:               "valid product",
+			path:               "/publish-product",
+			body:               []byte(`{"data":{"productId":"123","price":"599"}}`),
+			expectedStatusCode: http.StatusInternalServerError,
+		},
+	}
+
+	c := http.Client{}
+
+	for i, tc := range testCases {
+		req, _ := http.NewRequest(http.MethodPost, host+tc.path, bytes.NewBuffer(tc.body))
+		resp, err := c.Do(req)
+
+		assert.Equal(t, tc.expectedStatusCode, resp.StatusCode, "TEST[%d], Failed.\n%s", i, tc.desc)
+		assert.NoError(t, err, "TEST[%d], Failed.\n%s", i, tc.desc)
+
+		defer resp.Body.Close()
+	}
+}
