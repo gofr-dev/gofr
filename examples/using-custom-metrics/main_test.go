3d5d19d9aae0fec72887d7f7ffdb2b95010d69d7
Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>
2024-03-01 13:18:02 +0530

    Add custom metrics integration test (#343)
    * increase example test coverage
    
    * add comment to check if setEnv configs are being read
    
    * skip failure case for now
    
    * remove publisher tests
    
    * sort imports
    
    * fix successfully spelling
    
    * add intergration test for custom metrics
    
    * fix import sort
    
    ---------
    
    Co-authored-by: aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>

---
+++ examples/using-custom-metrics/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntegration(t *testing.T) {
+	const host = "http://localhost:9011"
+	go main()
+	time.Sleep(time.Second * 1) // Giving some time to start the server
+
+	c := http.Client{}
+
+	req, _ := http.NewRequest("POST", host+"/transaction", nil)
+
+	_, err := c.Do(req)
+	if err != nil {
+		t.Fatalf("request to /transaction failed %v", err)
+	}
+
+	req, _ = http.NewRequest("POST", host+"/return", nil)
+
+	_, err = c.Do(req)
+	if err != nil {
+		t.Fatalf("request to /transaction failed %v", err)
+	}
+
+	req, _ = http.NewRequest("GET", "http://localhost:2121/metrics", nil)
+
+	resp, err := c.Do(req)
+	if err != nil {
+		t.Fatalf("request to localhost:2121/metrics failed %v", err)
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+
+	strBody := string(body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "TEST[%d], Failed.\n%s")
+
+	assert.Contains(t, strBody, `product_stock{otel_scope_name="using-metrics",otel_scope_version="v0.1.0"} 50`)
+	assert.Contains(t, strBody, `total_credit_day_sale{otel_scope_name="using-metrics",otel_scope_version="v0.1.0",sale_type="credit"} 1000`)
+	assert.Contains(t, strBody, `total_credit_day_sale{otel_scope_name="using-metrics",otel_scope_version="v0.1.0",sale_type="credit_return"} -1000`)
+	assert.Contains(t, strBody, `transaction_success_total{otel_scope_name="using-metrics",otel_scope_version="v0.1.0"} 1`)
+	assert.Contains(t, strBody, "transaction_time")
+}
