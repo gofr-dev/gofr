49db06e21c5390501cb53b1f0b627d484a114de8
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-03-06 18:29:11 +0530

    add migration test

--- examples/using-migrations/migrations/all_test.go
+++ examples/using-migrations/migrations/all_test.go
@@ -15,6 +15,7 @@ func TestAll(t *testing.T) {
 	expected := map[int64]migration.Migrate{
 		1708322067: createTableEmployee(),
 		1708322089: addEmployeeInRedis(),
+		1708322090: createTopicsForStore(),
 	}
 
 	// Check if the length of the maps match
