03376226e4c68f384de7560a76eca2395c63f60e
Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>
2024-03-06 15:51:46 +0530

    HealthCheck for Pubsub - google and kafka (#361)
    * add pubsub healthcheck details

--- pkg/gofr/datasource/pubsub/kafka/interfaces.go
+++ pkg/gofr/datasource/pubsub/kafka/interfaces.go
@@ -9,9 +9,15 @@ import (
 type Reader interface {
 	ReadMessage(ctx context.Context) (kafka.Message, error)
 	CommitMessages(ctx context.Context, msgs ...kafka.Message) error
+	Stats() kafka.ReaderStats
 }
 
 type Writer interface {
 	WriteMessages(ctx context.Context, msg ...kafka.Message) error
 	Close() error
+	Stats() kafka.WriterStats
+}
+
+type Connection interface {
+	Controller() (broker kafka.Broker, err error)
 }
