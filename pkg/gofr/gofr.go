2ce5fc0d80914900ab7f09d08096f10617458c00
Umang Mundhra <mundhraumang.02@gmail.com>
2024-03-11 23:44:01 +0530

    Implement Basic Auth (#371)

--- pkg/gofr/gofr.go
+++ pkg/gofr/gofr.go
@@ -2,7 +2,6 @@ package gofr
 
 import (
 	"fmt"
-
 	"net/http"
 	"os"
 	"strconv"
@@ -275,6 +274,23 @@ type otelErrorHandler struct {
 
 func (o *otelErrorHandler) Handle(e error) {
 	o.logger.Error(e.Error())
+}
+
+func (a *App) EnableBasicAuth(credentials ...string) {
+	if len(credentials)%2 != 0 {
+		a.container.Error("Invalid number of arguments for EnableBasicAuth")
+	}
+
+	users := make(map[string]string)
+	for i := 0; i < len(credentials); i += 2 {
+		users[credentials[i]] = credentials[i+1]
+	}
+
+	a.httpServer.router.Use(middleware.BasicAuthMiddleware(middleware.BasicAuthProvider{Users: users}))
+}
+
+func (a *App) EnableBasicAuthWithFunc(validateFunc func(username, password string) bool) {
+	a.httpServer.router.Use(middleware.BasicAuthMiddleware(middleware.BasicAuthProvider{ValidateFunc: validateFunc}))
 }
 
 func (a *App) Subscribe(topic string, handler SubscribeFunc) {
