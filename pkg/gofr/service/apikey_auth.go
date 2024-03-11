ff8be395dc000eaf71293bf679c2fd8ca0962792
srijan-27 <srastogi751@gmail.com>
2024-03-11 21:48:57 +0530

    refactor duplicate code into setXApiKey function

--- pkg/gofr/service/apikey_auth.go
+++ pkg/gofr/service/apikey_auth.go
@@ -28,11 +28,7 @@ func (a *APIKeyAuthProvider) Get(ctx context.Context, path string, queryParams m
 
 func (a *APIKeyAuthProvider) GetWithHeaders(ctx context.Context, path string, queryParams map[string]interface{},
 	headers map[string]string) (*http.Response, error) {
-	if headers == nil {
-		headers = make(map[string]string)
-	}
-
-	headers["X-API-KEY"] = a.apiKey
+	setXApiKey(headers, a.apiKey)
 
 	return a.HTTP.GetWithHeaders(ctx, path, queryParams, headers)
 }
@@ -44,11 +40,7 @@ func (a *APIKeyAuthProvider) Post(ctx context.Context, path string, queryParams 
 
 func (a *APIKeyAuthProvider) PostWithHeaders(ctx context.Context, path string, queryParams map[string]interface{}, body []byte,
 	headers map[string]string) (*http.Response, error) {
-	if headers == nil {
-		headers = make(map[string]string)
-	}
-
-	headers["X-API-KEY"] = a.apiKey
+	setXApiKey(headers, a.apiKey)
 
 	return a.HTTP.PostWithHeaders(ctx, path, queryParams, body, headers)
 }
@@ -60,11 +52,7 @@ func (a *APIKeyAuthProvider) Put(ctx context.Context, api string, queryParams ma
 
 func (a *APIKeyAuthProvider) PutWithHeaders(ctx context.Context, path string, queryParams map[string]interface{}, body []byte,
 	headers map[string]string) (*http.Response, error) {
-	if headers == nil {
-		headers = make(map[string]string)
-	}
-
-	headers["X-API-KEY"] = a.apiKey
+	setXApiKey(headers, a.apiKey)
 
 	return a.HTTP.PutWithHeaders(ctx, path, queryParams, body, headers)
 }
@@ -76,11 +64,7 @@ func (a *APIKeyAuthProvider) Patch(ctx context.Context, path string, queryParams
 
 func (a *APIKeyAuthProvider) PatchWithHeaders(ctx context.Context, path string, queryParams map[string]interface{}, body []byte,
 	headers map[string]string) (*http.Response, error) {
-	if headers == nil {
-		headers = make(map[string]string)
-	}
-
-	headers["X-API-KEY"] = a.apiKey
+	setXApiKey(headers, a.apiKey)
 
 	return a.HTTP.PatchWithHeaders(ctx, path, queryParams, body, headers)
 }
@@ -91,11 +75,15 @@ func (a *APIKeyAuthProvider) Delete(ctx context.Context, path string, body []byt
 
 func (a *APIKeyAuthProvider) DeleteWithHeaders(ctx context.Context, path string, body []byte, headers map[string]string) (
 	*http.Response, error) {
+	setXApiKey(headers, a.apiKey)
+
+	return a.HTTP.DeleteWithHeaders(ctx, path, body, headers)
+}
+
+func setXApiKey(headers map[string]string, apiKey string) {
 	if headers == nil {
 		headers = make(map[string]string)
 	}
 
-	headers["X-API-KEY"] = a.apiKey
-
-	return a.HTTP.DeleteWithHeaders(ctx, path, body, headers)
+	headers["X-API-KEY"] = apiKey
 }
