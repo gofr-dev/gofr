8e38a48abc68911998d9aa3515e39a11df437543
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-03-20 18:02:46 +0530

    refactored example

--- examples/using-upload-handler/main.go
+++ examples/using-upload-handler/main.go
@@ -8,27 +8,34 @@ import (
 func main() {
 	app := gofr.New()
 
-	app.POST("/upload", func(c *gofr.Context) (interface{}, error) {
-		var data = struct {
-			File *file.Zip `file:"file"`
-		}{}
-
-		err := c.Bind(&data)
-		if err != nil {
-			return nil, err
-		}
-
-		if data.File == nil {
-			return "no file was uploaded", nil
-		}
-
-		err = data.File.CreateLocalCopies("tmp")
-		if err != nil {
-			return nil, err
-		}
-
-		return "Created", nil
-	})
+	app.POST("/upload", UploadHandler)
 
 	app.Run()
+}
+
+// Data is the struct that we are trying to bind files to
+type Data struct {
+	// The Compressed field is of type zip,
+	// the tag `upload` signifies the key for the form where the file is uploaded
+	// if the tag is not present, the field name would be taken as a key.
+	Compressed file.Zip `file:"upload"`
+}
+
+func UploadHandler(c *gofr.Context) (interface{}, error) {
+	var d Data
+
+	// bind the multipart data into the variable d
+	err := c.Bind(&d)
+	if err != nil {
+		return nil, err
+	}
+
+	// create local copies of the zipped files in tmp folder
+	err = d.Compressed.CreateLocalCopies("tmp")
+	if err != nil {
+		return nil, err
+	}
+
+	// return the number of compressed files recieved
+	return len(d.Compressed.Files), nil
 }
