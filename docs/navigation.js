fe4af787c1c7f677b928cb7b50986af0d39c2bb1
umang01-hash <mundhraumang.02@gmail.com>
2024-04-01 11:00:58 +0530

    add docs for crud from struct

--- docs/navigation.js
+++ docs/navigation.js
@@ -7,6 +7,7 @@ export const navigation = [
             { title: 'Connecting Redis', href: '/docs/quick-start/connecting-redis' },
             { title: 'Connecting MySQL', href: '/docs/quick-start/connecting-mysql' },
             { title: 'Observability', href: '/docs/quick-start/observability' },
+            { title: 'CRUD From Struct', href: '/docs/quick-start/crud-from-struct' },
         ],
     },
     {
