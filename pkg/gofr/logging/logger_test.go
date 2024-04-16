2b280cb0678c9f14a06336494b362bca77347359
Suyesh Badge <suyeshbadge@gmail.com>
2024-04-16 21:35:29 +0530

    Modified Tests with more cases

--- pkg/gofr/logging/logger_test.go
+++ pkg/gofr/logging/logger_test.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"bytes"
 	"encoding/json"
+	"reflect"
 
 	"io"
 	"os"
@@ -388,11 +389,89 @@ func Test_clean(t *testing.T) {
 	assert.Equal(t, expected, query)
 }
 
+// func TestDefaultFilterMaskingPII(t *testing.T) {
+// 	testCases := []struct {
+// 		name     string
+// 		input    interface{}
+// 		expected string
+// 	}{
+// 		{
+// 			name: "mask string fields",
+// 			input: struct {
+// 				Name     string `json:"name"`
+// 				Email    string `json:"email"`
+// 				Password string `json:"password"`
+// 			}{
+// 				Name:     "John Doe",
+// 				Email:    "john.doe@example.com",
+// 				Password: "secret123",
+// 			},
+// 			expected: `{"name":"********","email":"********************","password":"*********"}`,
+// 		},
+// 		{
+// 			name: "mask numeric fields",
+// 			input: struct {
+// 				PhoneNumber          int64   `json:"phoneNumber"`
+// 				SocialSecurityNumber int     `json:"socialSecurityNumber"`
+// 				CreditCardNumber     uint64  `json:"creditCardNumber"`
+// 				DateOfBirth          int     `json:"dateOfBirth"`
+// 				BiometricData        float64 `json:"biometricData"`
+// 				IPAddress            string  `json:"ipAddress"`
+// 			}{
+// 				PhoneNumber:          1234567890,
+// 				SocialSecurityNumber: 123456789,
+// 				CreditCardNumber:     1234567890123456,
+// 				DateOfBirth:          19800101,
+// 				BiometricData:        123.456,
+// 				IPAddress:            "192.168.1.1",
+// 			},
+// 			expected: `{"phoneNumber":0,"socialSecurityNumber":0,"creditCardNumber":0,"dateOfBirth":0,"biometricData":0,"ipAddress":"***********"}`,
+// 		},
+// 		// Add more test cases as needed
+// 	}
+
+// 	filter := &DefaultFilter{
+// 		MaskFields: []string{
+// 			"name",
+// 			"email",
+// 			"password",
+// 			"phoneNumber",
+// 			"socialSecurityNumber",
+// 			"creditCardNumber",
+// 			"dateOfBirth",
+// 			"biometricData",
+// 			"ipAddress",
+// 		},
+// 	}
+
+// 	for _, tc := range testCases {
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			entry := &logEntry{
+// 				Message: tc.input,
+// 			}
+
+// 			filteredEntry := filter.Filter(entry)
+// 			maskedMessage, err := json.Marshal(filteredEntry.Message)
+// 			if err != nil {
+// 				t.Errorf("Failed to marshal masked message: %v", err)
+// 				return
+// 			}
+
+// 			if string(maskedMessage) != tc.expected {
+// 				t.Errorf("Expected %s, but got %s", tc.expected, maskedMessage)
+// 			}
+// 		})
+// 	}
+// }
+
 func TestDefaultFilterMaskingPII(t *testing.T) {
 	testCases := []struct {
-		name     string
-		input    interface{}
-		expected string
+		name           string
+		input          interface{}
+		expected       string
+		enableMasking  bool
+		maskingFields  []string
+		expectedOutput interface{}
 	}{
 		{
 			name: "mask string fields",
@@ -405,7 +484,18 @@ func TestDefaultFilterMaskingPII(t *testing.T) {
 				Email:    "john.doe@example.com",
 				Password: "secret123",
 			},
-			expected: `{"name":"********","email":"********************","password":"*********"}`,
+			expected:      `{"name":"********","email":"********************","password":"**********"}`,
+			enableMasking: true,
+			maskingFields: []string{"name", "email", "password"},
+			expectedOutput: struct {
+				Name     string `json:"name"`
+				Email    string `json:"email"`
+				Password string `json:"password"`
+			}{
+				Name:     "********",
+				Email:    "********************",
+				Password: "**********",
+			},
 		},
 		{
 			name: "mask numeric fields",
@@ -424,33 +514,115 @@ func TestDefaultFilterMaskingPII(t *testing.T) {
 				BiometricData:        123.456,
 				IPAddress:            "192.168.1.1",
 			},
-			expected: `{"phoneNumber":0,"socialSecurityNumber":0,"creditCardNumber":0,"dateOfBirth":0,"biometricData":0,"ipAddress":"***********"}`,
+			expected:      `{"phoneNumber":0,"socialSecurityNumber":0,"creditCardNumber":0,"dateOfBirth":0,"biometricData":0,"ipAddress":"***********"}`,
+			enableMasking: true,
+			maskingFields: []string{"phoneNumber", "socialSecurityNumber", "creditCardNumber", "dateOfBirth", "biometricData", "ipAddress"},
+			expectedOutput: struct {
+				PhoneNumber          int64   `json:"phoneNumber"`
+				SocialSecurityNumber int     `json:"socialSecurityNumber"`
+				CreditCardNumber     uint64  `json:"creditCardNumber"`
+				DateOfBirth          int     `json:"dateOfBirth"`
+				BiometricData        float64 `json:"biometricData"`
+				IPAddress            string  `json:"ipAddress"`
+			}{
+				PhoneNumber:          0,
+				SocialSecurityNumber: 0,
+				CreditCardNumber:     0,
+				DateOfBirth:          0,
+				BiometricData:        0,
+				IPAddress:            "***********",
+			},
 		},
-		// Add more test cases as needed
-	}
-
-	filter := &DefaultFilter{
-		MaskFields: []string{
-			"name",
-			"email",
-			"password",
-			"phoneNumber",
-			"socialSecurityNumber",
-			"creditCardNumber",
-			"dateOfBirth",
-			"biometricData",
-			"ipAddress",
+		{
+			name:           "masking disabled",
+			input:          struct{ Name string }{Name: "John Doe"},
+			expected:       `{"Name":"John Doe"}`,
+			enableMasking:  false,
+			maskingFields:  []string{"name"},
+			expectedOutput: struct{ Name string }{Name: "John Doe"},
+		},
+		{
+			name: "mask fields in nested struct",
+			input: struct {
+				Name    string `json:"name"`
+				Email   string `json:"email"`
+				Address struct {
+					Street string `json:"street"`
+					City   string `json:"city"`
+					Zip    int    `json:"zip"`
+				} `json:"address"`
+				CreditCard struct {
+					Number string `json:"number"`
+					CVV    int    `json:"cvv"`
+				} `json:"creditCard"`
+			}{
+				Name:  "John Doe",
+				Email: "john.doe@example.com",
+				Address: struct {
+					Street string `json:"street"`
+					City   string `json:"city"`
+					Zip    int    `json:"zip"`
+				}{
+					Street: "123 Main St",
+					City:   "Anytown",
+					Zip:    12345,
+				},
+				CreditCard: struct {
+					Number string `json:"number"`
+					CVV    int    `json:"cvv"`
+				}{
+					Number: "1234567890123456",
+					CVV:    123,
+				},
+			},
+			expected:      `{"name":"********","email":"********************","address":{"street":"***********","city":"*******","zip":12345},"creditCard":{"number":"****************","cvv":0}}`,
+			enableMasking: true,
+			maskingFields: []string{"name", "email", "street", "number", "cvv", "city"},
+			expectedOutput: struct {
+				Name    string `json:"name"`
+				Email   string `json:"email"`
+				Address struct {
+					Street string `json:"street"`
+					City   string `json:"city"`
+					Zip    int    `json:"zip"`
+				} `json:"address"`
+				CreditCard struct {
+					Number string `json:"number"`
+					CVV    int    `json:"cvv"`
+				} `json:"creditCard"`
+			}{
+				Name:  "********",
+				Email: "********************",
+				Address: struct {
+					Street string `json:"street"`
+					City   string `json:"city"`
+					Zip    int    `json:"zip"`
+				}{
+					Street: "***********",
+					City:   "*******",
+					Zip:    12345,
+				},
+				CreditCard: struct {
+					Number string `json:"number"`
+					CVV    int    `json:"cvv"`
+				}{
+					Number: "****************",
+					CVV:    0,
+				},
+			},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			entry := &logEntry{
-				Message: tc.input,
+			filter := &DefaultFilter{
+				MaskFields:    tc.maskingFields,
+				EnableMasking: tc.enableMasking,
 			}
 
-			filteredEntry := filter.Filter(entry)
-			maskedMessage, err := json.Marshal(filteredEntry.Message)
+			filteredMessage := filter.Filter(tc.input)
+
+			maskedMessage, err := json.Marshal(filteredMessage)
 			if err != nil {
 				t.Errorf("Failed to marshal masked message: %v", err)
 				return
@@ -458,6 +630,10 @@ func TestDefaultFilterMaskingPII(t *testing.T) {
 
 			if string(maskedMessage) != tc.expected {
 				t.Errorf("Expected %s, but got %s", tc.expected, maskedMessage)
+			}
+
+			if !reflect.DeepEqual(filteredMessage, tc.expectedOutput) {
+				t.Errorf("Expected output %v, but got %v", tc.expectedOutput, filteredMessage)
 			}
 		})
 	}
