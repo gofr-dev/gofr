601ecaa5fb6c34bad8dc0bad1ed8a69dd6075b71
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2024-04-29 18:36:24 +0530

    fix pretty print and standardize spacing for all logs

--- pkg/gofr/datasource/pubsub/log.go
+++ pkg/gofr/datasource/pubsub/log.go
@@ -7,14 +7,15 @@ import (
 
 type Log struct {
 	Mode          string `json:"mode"`
-	MessageID     string `json:"messageID"`
+	CorrelationID string `json:"correlationID"`
 	MessageValue  string `json:"messageValue"`
 	Topic         string `json:"topic"`
 	Host          string `json:"host"`
 	PubSubBackend string `json:"pubSubBackend"`
+	Time          int64  `json:"time"`
 }
 
 func (l *Log) PrettyPrint(writer io.Writer) {
-	fmt.Fprintf(writer, "\u001B[38;5;8m%-32s \u001B[38;5;24m%s\u001b[0m            %-4s %s \u001b[38;5;101m%s\u001b[0m\n",
-		l.MessageID, l.PubSubBackend, l.Mode, l.Topic, l.MessageValue)
+	fmt.Fprintf(writer, "\u001B[38;5;8m%-32s \u001B[38;5;24m%-6s\u001B[0m %8d\u001B[38;5;8mµs\u001B[0m %-4s %s \u001b[38;5;101m%s\u001b[0m\n",
+		l.CorrelationID, l.PubSubBackend, l.Time, l.Mode, l.Topic, l.MessageValue)
 }
