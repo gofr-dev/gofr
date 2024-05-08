a8fe1955fb2b7c077abd70a9eccb08069a76e53a
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-05-08 22:23:58 +0530

    add file support

---
+++ pkg/gofr/datasource/file/ftp/ftp.go
@@ -0,0 +1,69 @@
+package ftp
+
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+
+	"github.com/jlaffaye/ftp"
+
+	"gofr.dev/pkg/gofr/datasource/file"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	UserName string
+	Password string
+
+	client *ftp.ServerConn
+}
+
+func New(config Config) (file.File, error) {
+	conn, err := ftp.Dial(fmt.Sprintf("%v:%v", config.Host, config.Port))
+	if err != nil {
+		return nil, err
+	}
+
+	config.client = conn
+
+	return config, nil
+}
+
+func (c Config) CreateDir(path string) error {
+	return c.client.MakeDir(path)
+}
+
+func (c Config) Create(path string, data []byte) error {
+	return nil
+}
+
+func (c Config) Update(path string, data []byte) error {
+	err := c.client.Rename(path, "temp_"+path)
+	if err != nil {
+		return err
+	}
+
+	err = c.client.Stor(path, bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c Config) Delete(path string) error {
+	return nil
+}
+
+func (c Config) Read(name string) ([]byte, error) {
+	return nil, nil
+}
+
+func (c Config) Move(src string, dest string) error {
+	return nil
+}
+
+func (c Config) Stat(name string) (fs.FileInfo, error) {
+	return nil, nil
+}
---
+++ pkg/gofr/datasource/file/local/file.go
@@ -0,0 +1,118 @@
+package local
+
+import (
+	"fmt"
+	"gofr.dev/pkg/gofr/datasource"
+	"io"
+	"io/fs"
+	"os"
+
+	"gofr.dev/pkg/gofr/datasource/file"
+)
+
+type local struct {
+	datasource.Logger
+}
+
+func New() file.File {
+	return local{}
+}
+
+func (c local) CreateDir(name string) error {
+	return os.MkdirAll(name, fs.ModeDir)
+}
+
+// Create creates the file named path along with any necessary parents,
+// and writes the given data to it.
+// If file exists, error is returned.
+// If file does not exist, it is created with mode 0666
+// Error return are of type *fs.PathError.
+// name contains the file name along with the path.
+func (c local) Create(name string, data []byte) error {
+	// Open the file for writing with exclusive creation flag
+	// os.O_WRONLY: Opens the file for writing only.
+	// os.O_CREATE: Creates the file if it doesn't exist.
+	// os.O_EXCL: Fails if the file already exists (exclusive creation).
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		// Handle existing file error or other errors
+		if os.IsExist(err) {
+			c.Logger.Errorf("file %v already exists: %v", name, err)
+			return err
+		}
+
+		return err
+	}
+
+	defer f.Close()
+
+	// Write data to the file
+	_, err = f.Write(data)
+	if err != nil {
+		c.Logger.Errorf("error writing data to file: %v", err)
+
+		return err
+	}
+
+	return nil
+}
+
+// Read reads the content of file and writes it in data.
+// If there is an error, it will be of type *fs.PathError.
+// name contains the file name along with the path.
+func (c local) Read(path string) ([]byte, error) {
+	// Open the file for reading
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err // Return error if opening fails
+	}
+	defer f.Close() // Close the file even on errors
+
+	// Allocate buffer for reading the file
+	data, err := io.ReadAll(f)
+	if err != nil {
+		c.Logger.Errorf("error reading file: %w", err)
+		return nil, err
+	}
+
+	return data, nil // Return data and nil error on success
+}
+
+func (c local) Rename(src string, dest string) error {
+	return os.Rename(src, dest)
+}
+
+func (c local) Update(name string, data []byte) error {
+	// Open the file for writing with truncation
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		// Handle file existence error or other errors
+		if os.IsNotExist(err) {
+			c.Errorf("file does not exist: %s", name)
+			return err
+		}
+		return err
+	}
+	defer f.Close()
+
+	// Write data to the file
+	_, err = f.Write(data)
+	if err != nil {
+		return fmt.Errorf("error writing data to file: %s", err)
+	}
+
+	// Success
+	return nil
+}
+
+func (c local) Delete(path string) error {
+	return nil
+}
+
+func (c local) Move(src string, dest string) error {
+	return nil
+}
+
+func (c local) Stat(name string) (fs.FileInfo, error) {
+	return nil, nil
+}
