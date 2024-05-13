39203c3b895077e6454ae157c3666efd5b9fe5ee
vipul-rawat <vipul.rawat@gofr.dev>
2024-05-13 13:21:08 +0530

    add PATCH method to cors Allow methods

--- pkg/gofr/http/middleware/cors.go
+++ pkg/gofr/http/middleware/cors.go
@@ -7,7 +7,7 @@ func CORS() func(inner http.Handler) http.Handler {
 	return func(inner http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 
 			if r.Method == http.MethodOptions {
 				w.WriteHeader(http.StatusOK)
