5df0b9a505fdcc6e3e3d5632ae6b9068237a6a58
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-05-20 18:43:19 +0530

    remove unused code

--- pkg/gofr/externalDB.go
+++ pkg/gofr/externalDB.go
@@ -2,12 +2,6 @@ package gofr
 
 import "gofr.dev/pkg/gofr/datasource"
 
-// builder has been created such that dependencies for external database can be injected.
-// without making the user aware for the same.
-type builder interface {
-	Build(o ...interface{})
-}
-
 func (a *App) AddMongo(db datasource.MongoProvider) {
 	db.UseLogger(a.Logger())
 	db.UseMetrics(a.Metrics())
