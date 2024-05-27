3d305478a00651cc06c8251b4467950f1fbfc180
Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>
2024-05-27 10:39:47 +0530

    make use mongo deprecated (#649)

--- pkg/gofr/externalDB.go
+++ pkg/gofr/externalDB.go
@@ -10,3 +10,9 @@ func (a *App) AddMongo(db datasource.MongoProvider) {
 
 	a.container.Mongo = db
 }
+
+// UseMongo sets the Mongo datasource in the app's container.
+// Deprecated: Use the NewMongo function AddMongo instead.
+func (a *App) UseMongo(db datasource.Mongo) {
+	a.container.Mongo = db
+}
