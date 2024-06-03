ca4bf2bbcd432c6f972ac493a67aedffce3f2b31
Umang Mundhra <mundhraumang.02@gmail.com>
2024-06-03 18:13:13 +0530

    Add support for custom cors header value(#669)
    Co-authored-by: Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>

--- pkg/gofr/http/router_test.go
+++ pkg/gofr/http/router_test.go
@@ -3,82 +3,66 @@ package http
 import (
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"gofr.dev/pkg/gofr/config"
 	"gofr.dev/pkg/gofr/container"
-	"gofr.dev/pkg/gofr/testutil"
 )
 
 func TestRouter(t *testing.T) {
-	log := testutil.StdoutOutputForFunc(func() {
-		cfg := map[string]string{"HTTP_PORT": "8000", "LOG_LEVEL": "INFO"}
-		c := container.NewContainer(config.NewMockConfig(cfg))
+	cfg := map[string]string{"HTTP_PORT": "8000", "LOG_LEVEL": "INFO"}
+	c := container.NewContainer(config.NewMockConfig(cfg))
 
-		c.Metrics().NewCounter("test-counter", "test")
+	c.Metrics().NewCounter("test-counter", "test")
 
-		// Create a new router instance using the mock container
-		router := NewRouter(c)
+	// Create a new router instance using the mock container
+	router := NewRouter()
 
-		// Add a test handler to the router
-		router.Add("GET", "/test", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}))
+	// Add a test handler to the router
+	router.Add("GET", "/test", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
 
-		// Send a request to the test handler
-		req := httptest.NewRequest("GET", "/test", http.NoBody)
-		rec := httptest.NewRecorder()
-		router.ServeHTTP(rec, req)
+	// Send a request to the test handler
+	req := httptest.NewRequest("GET", "/test", http.NoBody)
+	rec := httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
 
-		// Verify the response
-		assert.Equal(t, http.StatusOK, rec.Code)
-	})
-
-	// verify if middleware logger is properly functioning inside new router
-	if !strings.Contains(log, "\"method\":\"GET\",\"ip\":\"192.0.2.1:1234\",\"uri\":\"/test\",\"response\":200") {
-		t.Errorf("TestRouter Failed! expected log not found: %v", log)
-	}
+	// Verify the response
+	assert.Equal(t, http.StatusOK, rec.Code)
 }
 
 func TestRouterWithMiddleware(t *testing.T) {
-	log := testutil.StdoutOutputForFunc(func() {
-		cfg := map[string]string{"HTTP_PORT": "8000", "LOG_LEVEL": "INFO"}
-		c := container.NewContainer(config.NewMockConfig(cfg))
+	cfg := map[string]string{"HTTP_PORT": "8000", "LOG_LEVEL": "INFO"}
+	c := container.NewContainer(config.NewMockConfig(cfg))
 
-		c.Metrics().NewCounter("test-counter", "test")
+	c.Metrics().NewCounter("test-counter", "test")
 
-		// Create a new router instance using the mock container
-		router := NewRouter(c)
+	// Create a new router instance using the mock container
+	router := NewRouter()
 
-		router.UseMiddleware(func(inner http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("X-Test-Middleware", "applied")
-				inner.ServeHTTP(w, r)
-			})
+	router.UseMiddleware(func(inner http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("X-Test-Middleware", "applied")
+			inner.ServeHTTP(w, r)
 		})
-
-		// Add a test handler to the router
-		router.Add("GET", "/test", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}))
-
-		// Send a request to the test handler
-		req := httptest.NewRequest("GET", "/test", http.NoBody)
-		rec := httptest.NewRecorder()
-		router.ServeHTTP(rec, req)
-
-		// Verify the response
-		assert.Equal(t, http.StatusOK, rec.Code)
-		// checking if the testMiddleware has added the required header in the response properly.
-		testHeaderValue := rec.Header().Get("X-Test-Middleware")
-		assert.Equal(t, "applied", testHeaderValue, "Test_UseMiddleware Failed! header value mismatch.")
 	})
 
-	// verify if middleware logger is properly functioning inside new router
-	if !strings.Contains(log, "\"method\":\"GET\",\"ip\":\"192.0.2.1:1234\",\"uri\":\"/test\",\"response\":200") {
-		t.Errorf("TestRouter Failed! expected log not found: %v", log)
-	}
+	// Add a test handler to the router
+	router.Add("GET", "/test", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	// Send a request to the test handler
+	req := httptest.NewRequest("GET", "/test", http.NoBody)
+	rec := httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+
+	// Verify the response
+	assert.Equal(t, http.StatusOK, rec.Code)
+	// checking if the testMiddleware has added the required header in the response properly.
+	testHeaderValue := rec.Header().Get("X-Test-Middleware")
+	assert.Equal(t, "applied", testHeaderValue, "Test_UseMiddleware Failed! header value mismatch.")
 }
