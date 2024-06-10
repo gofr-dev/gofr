b825b8897042461882e6088c8bdedac20469e25c
Aryan Mehrotra <44036979+aryanmehrotra@users.noreply.github.com>
2024-06-10 15:09:25 +0530

    Add support for local file store (#695)

---
+++ pkg/gofr/datasource/file/fs.go
@@ -0,0 +1,63 @@
+package file
+
+import (
+	"os"
+
+	"gofr.dev/pkg/gofr/datasource"
+)
+
+type fileSystem struct {
+	logger datasource.Logger
+}
+
+// New initializes local filesystem with logger.
+func New(logger datasource.Logger) datasource.FileSystem {
+	return fileSystem{logger: logger}
+}
+
+func (f fileSystem) Create(name string) (datasource.File, error) {
+	newFile, err := os.Create(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &file{File: newFile, logger: f.logger}, nil
+}
+
+func (f fileSystem) Mkdir(name string, perm os.FileMode) error {
+	return os.Mkdir(name, perm)
+}
+
+func (f fileSystem) MkdirAll(path string, perm os.FileMode) error {
+	return os.MkdirAll(path, perm)
+}
+
+func (f fileSystem) Open(name string) (datasource.File, error) {
+	openFile, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &file{File: openFile, logger: f.logger}, nil
+}
+
+func (f fileSystem) OpenFile(name string, flag int, perm os.FileMode) (datasource.File, error) {
+	openFile, err := os.OpenFile(name, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+
+	return &file{File: openFile, logger: f.logger}, nil
+}
+
+func (f fileSystem) Remove(name string) error {
+	return os.Remove(name)
+}
+
+func (f fileSystem) RemoveAll(path string) error {
+	return os.RemoveAll(path)
+}
+
+func (f fileSystem) Rename(oldname, newname string) error {
+	return os.Rename(oldname, newname)
+}
