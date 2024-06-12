373d5a585b3839c26158f01462c613a712b36178
Aditya Nandan <59480626+aditya028@users.noreply.github.com>
2024-06-12 12:40:30 +0530

    fix every cmd response is followed by % (#697)

--- pkg/gofr/gofr_test.go
+++ pkg/gofr/gofr_test.go
@@ -29,9 +29,8 @@ func TestNewCMD(t *testing.T) {
 	a := NewCMD()
 	// Without args we should get error on stderr.
 	outputWithoutArgs := testutil.StderrOutputForFunc(a.Run)
-	if outputWithoutArgs != "No Command Found!" {
-		t.Errorf("Stderr output mismatch. Got: %s ", outputWithoutArgs)
-	}
+
+	assert.Equal(t, "No Command Found!\n", outputWithoutArgs, "TEST Failed.\n%s", "Stderr output mismatch")
 }
 
 func TestGofr_readConfig(t *testing.T) {
