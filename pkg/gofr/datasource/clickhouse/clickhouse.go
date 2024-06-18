beed80889a218a070ce4b861092fe4ab45cf1bdf
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-06-18 13:26:02 +0530

    add logs for db connection

--- pkg/gofr/datasource/clickhouse/clickhouse.go
+++ pkg/gofr/datasource/clickhouse/clickhouse.go
@@ -54,6 +54,8 @@ func (c *client) UseMetrics(metrics interface{}) {
 func (c *client) Connect() {
 	var err error
 
+	c.logger.Logf("connecting to clickhouse db at %v to database %v", c.config.Hosts, c.config.Database)
+
 	clickHouseBuckets := []float64{.05, .075, .1, .125, .15, .2, .3, .5, .75, 1, 2, 3, 4, 5, 7.5, 10}
 	c.metrics.NewHistogram("app_clickhouse_stats", "Response time of Clickhouse queries in milliseconds.", clickHouseBuckets...)
 
@@ -80,6 +82,8 @@ func (c *client) Connect() {
 
 	if err = c.conn.Ping(ctx); err != nil {
 		c.logger.Errorf("ping failed with error %v", err)
+	} else {
+		c.logger.Logf("successfully connected to clickhouseDB")
 	}
 
 	go pushDBMetrics(c.conn, c.metrics)
