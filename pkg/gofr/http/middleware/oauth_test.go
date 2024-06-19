3a232ff2783146f30c122002698569c215b374b4
umang01-hash <mundhraumang.02@gmail.com>
2024-06-19 17:45:11 +0530

    fix failing oAuth test

--- pkg/gofr/http/middleware/oauth_test.go
+++ pkg/gofr/http/middleware/oauth_test.go
@@ -24,15 +24,14 @@ func TestOAuthSuccess(t *testing.T) {
 	server := httptest.NewServer(router)
 
 	req, _ := http.NewRequestWithContext(context.Background(), http.MethodGet, server.URL+"/test", http.NoBody)
-	req.Header.Set("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IklDbmFZdEwtSDExckl0WlJ4VVlLVElzbm"+
-		"5ybm1wWUp6cGFWRHVDRWN0Ukk9IiwidHlwIjoiSldUIn0.eyJhdWQiOiJzdGFnZS5rb3BzLmRldiIsImV4cCI6MTcxODc5MjQ2NiwiaWF0Ij"+
-		"oxNzEwMTUyNDY2LCJpc3MiOiJzdGFnZS5hdXRoLnpvcHNtYXJ0LmNvbSIsIm5hbWUiOiJSYWtzaGl0IFNpbmdoIiwib3JpZyI6IkdPT0dMRSI"+
-		"sInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLSjVEREE0enJ1ekZsc1E5S3ZMakhEdGJPVF9o"+
-		"cFZ6MGhFTzhqU2wybTdNeWs9czk2LWMiLCJzdWIiOiJyYWtzaGl0LnNpbmdoQHpvcHNtYXJ0LmNvbSIsInN1Yi1pZCI6ImE2NTczZTFkLWFiZW"+
-		"EtNDg2My1hY2RiLTZjZjM2MjZhNDQxNCIsInR5cCI6InJlZnJlc2hfdG9rZW4ifQ.eoRVSFcyvbWk-fUSlACI4pWwHcuwjA1BbKlYA_aEJA6T"+
-		"BRcnM0HoaxL_GcF0Q-95Z6Medk9l5Fe-zuY4xmLX0XRnA9y9KEsXvyhxsmLJTV32C2kirDh6TR5FIep3EKV0VdWKJT6LziBjrCP-F0pKb34em"+
-		"Ua7gsyi5OnkX12_ZcGpQpSbL3mcZpEEGUmKijlg1VspK4G9dTmNSUXofxStokxacLwa3hiFfkd7vtegkF79bfWPVm0hlJDGDcU7szUaIyHjdW"+
-		"rlUGqQ0A8-8dYQ-Z1o5STZITcxvSv6SaZNo08r_szi-TDLXRhASP3ojEjFCqFBmPw9HPxHG4JmV3SX2A")
+	req.Header.Set("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjAwVFEwdlRpNVB1UnZscUZGY3dCeUc0WjBM"+
+		"dGREcUtJX0JWUFRrdnpleEUiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJzdGFnZS5rb3BzLmRldiIsImlhdCI6MTI1Nzg5NDAwMCwib3JpZyI6IkdP"+
+		"T0dMRSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLSjVEREE0enJ1ekZsc1E5S3ZMakhEdG"+
+		"JPVF9ocFZ6MGhFTzhqU2wybTdNeWs9czk2LWMiLCJzdWIiOiJyYWtzaGl0LnNpbmdoQHpvcHNtYXJ0LmNvbSIsInN1Yi1pZCI6ImE2NTczZTFkL"+
+		"WFiZWEtNDg2My1hY2RiLTZjZjM2MjZhNDQxNCIsInR5cCI6InJlZnJlc2hfdG9rZW4ifQ.NkYSi6KJtGA3js9dcN3UqJWfeJdB88p7cxclrc6"+
+		"fxJODlCalsbbwIr3QL4AR9i0ucJjmoTIipCwpdM1IYDjCd-ilf2mTp11Wba31XoH--8YLI9Ju0wbpYhtF3wa00NF1Ijt48ze09IJ6QtE-etm"+
+		"AN8T7izsXbPeSrFiN3NVQU87eGxc3bEQhEsV5u3E6j8EdVDv8xbwisETY-N0mDftZp0w8UCkQ7MarOrA5IaXs2MHyCETy5y9QFd4djppH9oFo"+
+		"y5-AtEZqzyHKfGMlerjtJp8uOgFso9FycGuO0TFhR4AaZGVZxB072Hu-71tbx7atXp3zmDdkK_jkg5aVepoU_Q")
 
 	client := http.Client{}
 
@@ -274,14 +273,14 @@ func (m *MockProvider) GetWithHeaders(context.Context, string, map[string]interf
 	responseBody := map[string]interface{}{
 		"keys": []map[string]string{
 			{
-				"kid": "ICnaYtL-H11rItZRxUYKTIsnnrnmpYJzpaVDuCEctRI=",
-				"n": "m1_RTyr57vrdWV-u4yxOt1Vq-adbM0njVXa4nNMDG5hBEaIzkRaNzailyO38oa8byf0EAlqrpo7n4MdascXt5WzthSnG7" +
-					"kVa5r7BnPf4OLkvw9YmecytJNVwuM37Wk-53yqsDHqa7wHEhrIj-4zkB0ssN9ya2V_kZoCfICxBuWhlqry0H0jcdn4n84bbp4v" +
-					"b7JI9zcJ17iuIZcagzLHRxZ95wieP1bxxkcbDOg8lXeJYRTkAjiPwBnQqlMQlJXxIFq0Ow8qmRPtf-p5ZqJuR74LnZ5KxlrpTE-" +
-					"wx0pvvd0-dmESVbdLn6LR-Ww-jl96aKWX-QiZWROrlKeda5r1LZw",
-				"e":   "AQAB",
-				"use": "sig",
 				"kty": "RSA",
+				"use": "sig",
+				"kid": "00TQ0vTi5PuRvlqFFcwByG4Z0LtdDqKI_BVPTkvzexE",
+				"n": "0nb5fKw3xb4_NMkEh80jG0_HuKByBnTIRqPTX-xbtSEDTsev1O4oyl3az0UdebyimwqHSLPVFIitHnfhHsto0IycnL9omEm" +
+					"40YWEUxOqs5HJaFhZsKHZmxCUkYsb-nHhYm67sYiPkcBQrisWFJi4r48EyLv050D85MkhPiD3Iy0Q5m29U-Hf9CIfxy1MS8akJ" +
+					"uTnk8Ir4ajHN7ze33IOAjE1UPX1viZ6QSwbFPo0YrGf6vZq21cbhS6UD1JC-A_iFVdSGKzBAfFspQaAllifmaym6XK-q4mKqTW" +
+					"430zKlGCnQd3ddg3zmCe7KqpJ6aDVUQ0FS_K8GnOoWeScWEj0qw",
+				"e": "AQAB",
 			},
 		},
 	}
