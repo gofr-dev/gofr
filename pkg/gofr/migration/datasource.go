6fe457ce1d8fa48fc48cdd1e909990e8dee6a6d5
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-06-21 17:54:19 +0530

    remove clickhouse changes

--- pkg/gofr/migration/datasource.go
+++ pkg/gofr/migration/datasource.go
@@ -1,13 +1,10 @@
 package migration
 
-import "gofr.dev/pkg/gofr/datasource"
-
 type Datasource struct {
 	// TODO this should not be embedded rather it should be
 	Logger
 
-	SQL        SQL
-	Redis      Redis
-	PubSub     Client
-	Clickhouse datasource.Clickhouse
+	SQL    SQL
+	Redis  Redis
+	PubSub PubSub
 }
