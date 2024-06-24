50a32b06a86f0bd84ed8407d5191a8c318e32aff
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-06-24 23:12:34 +0530

    fix linter

--- pkg/gofr/migration/datasource.go
+++ pkg/gofr/migration/datasource.go
@@ -15,22 +15,27 @@ type Datasource struct {
 
 // It is a base implementation for migration manger, on this other database drivers have been wrapped.
 
+//nolint:gocritic // Datasource has to be changed to a pointer in a different PR.
 func (d Datasource) checkAndCreateMigrationTable(*container.Container) error {
 	return nil
 }
 
+//nolint:gocritic // Datasource has to be changed to a pointer in a different PR.
 func (d Datasource) getLastMigration(*container.Container) int64 {
 	return 0
 }
 
+//nolint:gocritic // Datasource has to be changed to a pointer in a different PR.
 func (d Datasource) beginTransaction(*container.Container) transactionData {
 	return transactionData{}
 }
 
+//nolint:gocritic // Datasource has to be changed to a pointer in a different PR.
 func (d Datasource) commitMigration(c *container.Container, data transactionData) error {
 	c.Infof("Migration %v ran successfully", data.MigrationNumber)
 
 	return nil
 }
 
+//nolint:gocritic // Datasource has to be changed to a pointer in a different PR.
 func (d Datasource) rollback(*container.Container, transactionData) {}
