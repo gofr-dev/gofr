0227da7b457bc4f8da0e6cabab0973e43ae7a1b3
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-06-24 15:19:30 +0530

    revert pkg/gofr/service changes

--- pkg/gofr/service/oauth_test.go
+++ pkg/gofr/service/oauth_test.go
@@ -53,7 +53,7 @@ func setupHTTPServiceTestServerForOAuth(server *httptest.Server) HTTP {
 		},
 	}
 
-	// apply circuit breaker option to the HTTP service
+	// Apply option to the HTTP service
 	httpSvc := oauthConfig.AddOption(&service)
 
 	return httpSvc
@@ -70,7 +70,7 @@ func setupHTTPServiceTestServerForOAuthWithUnSupportedMethod() HTTP {
 	// Circuit breaker configuration
 	oauthConfig := OAuthConfig{}
 
-	// apply circuit breaker option to the HTTP service
+	// Apply circuit breaker option to the HTTP service
 	httpSvc := oauthConfig.AddOption(&service)
 
 	return httpSvc
