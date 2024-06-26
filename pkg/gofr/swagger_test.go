f2d186e725526e40aa82ec2f7a2da7de72aa98a8
mehrotra234 <aryanmehrotra2000@gmail.com>
2024-06-26 14:01:23 +0530

    keep inline

--- pkg/gofr/swagger_test.go
+++ pkg/gofr/swagger_test.go
@@ -79,8 +79,7 @@ func TestSwaggerHandler(t *testing.T) {
 	}{
 		{"fetch index.html", "", "text/html"},
 		{"fetch favicon image", "favicon-16x16.png", "image/png"},
-		{"fetch js files", "swagger-ui.js", "text" +
-			"/javascript"},
+		{"fetch js files", "swagger-ui.js", "text/javascript"},
 	}
 
 	for _, tc := range tests {
