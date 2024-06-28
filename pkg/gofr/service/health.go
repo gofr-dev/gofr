5c350679684ceb7dda9ad4a1db278cb07a5b8804
vipul-rawat <vipul.rawat@gofr.dev>
2024-06-28 10:58:15 +0530

    add request timeout for health check calls, add timeout field for custom health config

--- pkg/gofr/service/health.go
+++ pkg/gofr/service/health.go
@@ -3,11 +3,13 @@ package service
 import (
 	"context"
 	"net/http"
+	"time"
 )
 
 const (
-	serviceUp   = "UP"
-	serviceDown = "DOWN"
+	serviceUp      = "UP"
+	serviceDown    = "DOWN"
+	defaultTimeout = 5
 )
 
 type Health struct {
@@ -16,14 +18,20 @@ type Health struct {
 }
 
 func (h *httpService) HealthCheck(ctx context.Context) *Health {
-	return h.getHealthResponseForEndpoint(ctx, ".well-known/alive")
+	return h.getHealthResponseForEndpoint(ctx, ".well-known/alive", defaultTimeout)
 }
 
-func (h *httpService) getHealthResponseForEndpoint(ctx context.Context, endpoint string) *Health {
+func (h *httpService) getHealthResponseForEndpoint(_ context.Context, endpoint string, timeout int) *Health {
 	var healthResponse = Health{
 		Details: make(map[string]interface{}),
 	}
 
+	// caeate a new context with 5 second timeout for healthCheck call.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+
+	// send a new context as we can have downstream services taking too long
+	// which may cancel the original health check http request
 	resp, err := h.Get(ctx, endpoint, nil)
 
 	if err != nil || resp == nil {
