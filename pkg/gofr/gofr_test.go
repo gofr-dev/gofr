0a6434d8d4ec0653a4cb1a495152c1ad1c58e23e
srijan-27 <srastogi751@gmail.com>
2024-07-09 11:44:21 +0530

    add auth header support for tracer exporter

--- pkg/gofr/gofr_test.go
+++ pkg/gofr/gofr_test.go
@@ -337,17 +337,33 @@ func Test_initTracer(t *testing.T) {
 		"TRACE_EXPORTER": "gofr",
 	})
 
+	mockConfig4 := config.NewMockConfig(map[string]string{
+		"TRACE_EXPORTER":  "zipkin",
+		"TRACER_HOST":     "localhost",
+		"TRACER_PORT":     "2005",
+		"TRACER_AUTH_KEY": "valid-token",
+	})
+
+	mockConfig5 := config.NewMockConfig(map[string]string{
+		"TRACE_EXPORTER":  "jaeger",
+		"TRACER_HOST":     "localhost",
+		"TRACER_PORT":     "2005",
+		"TRACER_AUTH_KEY": "valid-token",
+	})
+
 	tests := []struct {
 		desc               string
 		config             config.Config
 		expectedLogMessage string
 	}{
 		{"zipkin exporter", mockConfig1, "Exporting traces to zipkin."},
+		{"zipkin exporter with auth", mockConfig4, "Exporting traces to zipkin."},
 		{"jaeger exporter", mockConfig2, "Exporting traces to jaeger."},
+		{"jaeger exporter with auth", mockConfig5, "Exporting traces to jaeger."},
 		{"gofr exporter", mockConfig3, "Exporting traces to GoFr at https://tracer.gofr.dev"},
 	}
 
-	for _, tc := range tests {
+	for i, tc := range tests {
 		logMessage := testutil.StdoutOutputForFunc(func() {
 			mockContainer, _ := container.NewMockContainer(t)
 
@@ -359,7 +375,7 @@ func Test_initTracer(t *testing.T) {
 			a.initTracer()
 		})
 
-		assert.Contains(t, logMessage, tc.expectedLogMessage)
+		assert.Contains(t, logMessage, tc.expectedLogMessage, "TEST[%d], Failed.\n%s", i, tc.desc)
 	}
 }
 
