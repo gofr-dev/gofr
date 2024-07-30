0297b4b07ee8b73a7bdc0a0aae1990e2498ce8d3
umang01-hash <mundhraumang.02@gmail.com>
2024-07-30 14:23:44 +0530

    reset sleep time in migration and subscription example

--- examples/using-migrations/main_test.go
+++ examples/using-migrations/main_test.go
@@ -12,7 +12,7 @@ import (
 func TestExampleMigration(t *testing.T) {
 	const host = "http://localhost:9100"
 	go main()
-	time.Sleep(100 * time.Millisecond) // Giving some time to start the server
+	time.Sleep(3 * time.Second) // Giving some time to start the server
 
 	tests := []struct {
 		desc       string
--- examples/using-subscriber/main_test.go
+++ examples/using-subscriber/main_test.go
@@ -44,7 +44,7 @@ func TestExampleSubscriber(t *testing.T) {
 	log := testutil.StdoutOutputForFunc(func() {
 		const host = "http://localhost:8200"
 		go main()
-		time.Sleep(2 * time.Second) // TODO: reduce this time
+		time.Sleep(20 * time.Second) // TODO: reduce this time
 	})
 
 	testCases := []struct {
