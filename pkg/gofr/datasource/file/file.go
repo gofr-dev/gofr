6ede3883f8bfccafc3aeb8833d43bcef991011c5
Divya Darshana <98943137+coolwednesday@users.noreply.github.com>
2024-08-06 10:58:44 +0530

    Ftp Server (#871)

--- pkg/gofr/datasource/file/file.go
+++ pkg/gofr/datasource/file/file.go
@@ -48,7 +48,7 @@ type jsonReader struct {
 //		    var content string
 //		    reader.Scan(&u)
 //	}
-func (f file) ReadAll() (datasource.RowReader, error) {
+func (f file) ReadAll() (RowReader, error) {
 	if strings.HasSuffix(f.File.Name(), ".json") {
 		return f.createJSONReader()
 	}
@@ -57,7 +57,7 @@ func (f file) ReadAll() (datasource.RowReader, error) {
 }
 
 // Factory method to create the appropriate JSON reader.
-func (f file) createJSONReader() (datasource.RowReader, error) {
+func (f file) createJSONReader() (RowReader, error) {
 	decoder := json.NewDecoder(f.File)
 
 	token, err := f.peekJSONToken(decoder)
@@ -88,7 +88,7 @@ func (file) peekJSONToken(decoder *json.Decoder) (json.Token, error) {
 }
 
 // Create a JSON reader for a JSON object.
-func (f file) createJSONObjectReader() (datasource.RowReader, error) {
+func (f file) createJSONObjectReader() (RowReader, error) {
 	name := f.File.Name()
 
 	if err := f.File.Close(); err != nil {
@@ -107,7 +107,7 @@ func (f file) createJSONObjectReader() (datasource.RowReader, error) {
 	return &jsonReader{decoder: decoder}, nil
 }
 
-func (f file) createTextCSVReader() datasource.RowReader {
+func (f file) createTextCSVReader() RowReader {
 	return &textReader{
 		scanner: bufio.NewScanner(f.File),
 		logger:  f.logger,
