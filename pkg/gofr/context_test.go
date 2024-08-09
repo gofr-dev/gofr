26b917f269ee4646f737caa1d82d1a1b98bca214
Divya Darshana <divi229moc@gmail.com>
2024-08-09 13:48:51 +0530

    Adding tests for functiond in context.go
    Adding test functions for context.go file

--- pkg/gofr/context_test.go
+++ pkg/gofr/context_test.go
@@ -3,7 +3,9 @@ package gofr
 import (
 	"bytes"
 	"context"
+	"github.com/gorilla/websocket"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,4 +37,56 @@ func Test_newContextSuccess(t *testing.T) {
 
 	assert.Equal(t, map[string]string{"key": "value"}, body, "TEST Failed \n unable to read body")
 	require.NoError(t, err, "TEST Failed \n unable to read body")
+}
+
+func TestContext_Trace(t *testing.T) {
+	ctx := Context{
+		Context: context.Background(),
+	}
+
+	span := ctx.Trace("Some Work")
+	defer span.End()
+
+	assert.NotNil(t, span)
+}
+
+func TestContext_WriteMessageToSocket(t *testing.T) {
+	t.Setenv("HTTP_PORT", "8005")
+
+	app := New()
+
+	server := httptest.NewServer(app.httpServer.router)
+	defer server.Close()
+
+	app.WebSocket("/ws", func(ctx *Context) (interface{}, error) {
+		err := ctx.WriteMessageToSocket("Hello! GoFr")
+		if err != nil {
+			return nil, err
+		}
+
+		// returning error here to close the connection to the websocket
+		// as the websocket close error is not caught because we are using no bind function.
+		// this must not be necessary if we put an actual check in handleWebSocketConnection method.
+		return nil, &websocket.CloseError{Code: websocket.CloseNormalClosure, Text: "Error closing"}
+	})
+
+	go app.Run()
+
+	wsURL := "ws" + server.URL[len("http"):] + "/ws"
+
+	// Create a WebSocket client
+	ws, resp, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	_, message, err := ws.ReadMessage()
+	ws.Close()
+
+	require.NoError(t, err)
+
+	// Read the response
+	expectedResponse := "Hello! GoFr"
+	assert.Equal(t, expectedResponse, string(message))
+
 }
