449272d3e5bcf73a322c1bf8aaba376ce6abbb5a
Michael Freeman <mfreeman451@gmail.com>
2024-09-16 21:55:54 -0500

    🐛 accidently removed a log statement

--- pkg/gofr/datasource/pubsub/nats/metrics.go
+++ pkg/gofr/datasource/pubsub/nats/metrics.go
@@ -4,6 +4,7 @@ import "context"
 
 //go:generate mockgen -destination=mock_metrics.go -package=nats -source=./metrics.go
 
+// Metrics represents the metrics interface.
 type Metrics interface {
 	IncrementCounter(ctx context.Context, name string, labels ...string)
 }
