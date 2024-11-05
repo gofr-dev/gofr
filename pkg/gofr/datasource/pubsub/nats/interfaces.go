af351e2f41903fb79a6210cf24b13038d4440a72
umang01-hash <mundhraumang.02@gmail.com>
2024-11-05 11:51:11 +0530

    remove context as a parameter from Connect method to satisfy interface

--- pkg/gofr/datasource/pubsub/nats/interfaces.go
+++ pkg/gofr/datasource/pubsub/nats/interfaces.go
@@ -42,7 +42,7 @@ type JetStreamClient interface {
 
 // ConnectionManagerInterface represents the main Client connection.
 type ConnectionManagerInterface interface {
-	Connect(ctx context.Context) error
+	Connect() error
 	Close(ctx context.Context)
 	Publish(ctx context.Context, subject string, message []byte, metrics Metrics) error
 	Health() datasource.Health
