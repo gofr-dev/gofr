f4b351c502afaf14485a90b8fe350e56067558c4
Rigved Telang <rigvedtelang20@gmail.com>
2024-11-10 17:23:53 +0530

    #1130:SetCustomHeaders function in response.go

--- pkg/gofr/http/response/response.go
+++ pkg/gofr/http/response/response.go
@@ -1,6 +1,21 @@
 package response
 
+import (
+	"net/http"
+)
+
 type Response struct {
 	Data    any               `json:"data"`
 	Headers map[string]string `json:"-"`
+}
+
+func (resp Response) SetCustomHeaders(w http.ResponseWriter) {
+	for key, value := range resp.Headers {
+		if w.Header().Get(key) != "" {
+			// do not overwrite existing header
+			continue
+		} else {
+			w.Header().Set(key, value)
+		}
+	}
 }
