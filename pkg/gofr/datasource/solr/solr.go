3c95ad2cdcdb1ce865180cd6241548d352ffb01e
Divya Darshana <divi229moc@gmail.com>
2024-11-18 14:01:38 +0530

    changing time unit to microseconds from milliseconds to accurately display duration in logs

--- pkg/gofr/datasource/solr/solr.go
+++ pkg/gofr/datasource/solr/solr.go
@@ -274,7 +274,7 @@ func (c *Client) createRequest(ctx context.Context, method, url string, params m
 }
 
 func (c *Client) sendOperationStats(ctx context.Context, ql *QueryLog, startTime time.Time, method string, span trace.Span) {
-	duration := time.Since(startTime).Milliseconds()
+	duration := time.Since(startTime).Microseconds()
 
 	ql.Duration = duration
 
