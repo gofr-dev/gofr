35bbc7240a1147167af1e8f21c9e6d97609f6e3b
Divya Darshana <divi229moc@gmail.com>
2024-11-21 11:18:36 +0530

    removing exclude build commands for future addition of external datasource pipeline as well as fixing casing of logs

--- pkg/gofr/datasource/mongo/mongo_test.go
+++ pkg/gofr/datasource/mongo/mongo_test.go
@@ -42,8 +42,8 @@ func Test_NewMongoClientError(t *testing.T) {
 	metrics := NewMockMetrics(ctrl)
 	logger := NewMockLogger(ctrl)
 
-	logger.EXPECT().Debug(fmt.Sprintf("connecting to mongoDB at %v to database %v", "mongo", "test"))
-	logger.EXPECT().Errorf("error connecting to mongoDB, err:%v", gomock.Any())
+	logger.EXPECT().Debug(fmt.Sprintf("connecting to MongoDB at %v to database %v", "mongo", "test"))
+	logger.EXPECT().Errorf("error connecting to MongoDB, err:%v", gomock.Any())
 
 	client := New(Config{URI: "mongo", Database: "test"})
 	client.UseLogger(logger)
