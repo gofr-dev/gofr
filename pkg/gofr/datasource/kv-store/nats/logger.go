3a30122f5eeaef1f5c800e536526f01aac1d8f46
vaidehiadhi <vaidehiadhi2003@gmail.com>
2025-01-23 15:58:55 +0530

    looger and metrics

--- pkg/gofr/datasource/kv-store/nats/logger.go
+++ pkg/gofr/datasource/kv-store/nats/logger.go
@@ -1,1 +1,27 @@
 package nats
+
+import (
+	"fmt"
+	"io"
+)
+
+type Logger interface {
+	Debug(args ...any)
+	Debugf(pattern string, args ...any)
+	Info(args ...any)
+	Infof(pattern string, args ...any)
+	Error(args ...any)
+	Errorf(pattern string, args ...any)
+}
+
+type Log struct {
+	Type     string `json:"type"`
+	Duration int64  `json:"duration"`
+	Key      string `json:"key"`
+	Value    string `json:"value,omitempty"`
+}
+
+func (l *Log) PrettyPrint(writer io.Writer) {
+	fmt.Fprintf(writer, "\u001B[38;5;8m%-32s \u001B[38;5;162m%-6s\u001B[0m %8d\u001B[38;5;8mµs\u001B[0m %s \n",
+		l.Type, "NATS", l.Duration, l.Key+" "+l.Value)
+}
