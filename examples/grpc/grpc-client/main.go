8ce625ce315cb6689dce4d1473276d514138bf68
Divya Darshana <98943137+coolwednesday@users.noreply.github.com>
2025-01-27 09:49:55 +0530

    Update gRPC documentation and examples to include new metrics functionality (#1439)
    * adding metrics in gRPC documentation & examples
    
    * minor fix
    
    * minor fix
    
    ---------
    
    Co-authored-by: Umang Mundhra <mundhraumang.02@gmail.com>

--- examples/grpc/grpc-client/main.go
+++ examples/grpc/grpc-client/main.go
@@ -9,7 +9,7 @@ func main() {
 	app := gofr.New()
 
 	// Create a gRPC client for the Hello service
-	helloGRPCClient, err := client.NewHelloGoFrClient(app.Config.Get("GRPC_SERVER_HOST"))
+	helloGRPCClient, err := client.NewHelloGoFrClient(app.Config.Get("GRPC_SERVER_HOST"), app)
 	if err != nil {
 		app.Logger().Errorf("Failed to create Hello gRPC client: %v", err)
 		return
