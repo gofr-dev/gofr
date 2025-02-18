f1b3df27b9df0e118188df146bc5f355ac3248cf
Divya Darshana <98943137+coolwednesday@users.noreply.github.com>
2025-02-18 18:41:11 +0530

    Storing the number of running GoFr servers (#1474)

--- pkg/gofr/gofr.go
+++ pkg/gofr/gofr.go
@@ -42,6 +42,11 @@ const (
 	gofrTraceExporter      = "gofr"
 	gofrTracerURL          = "https://tracer.gofr.dev"
 	checkPortTimeout       = 2 * time.Second
+	gofrHost               = "https://gofr.dev"
+	startServerPing        = "/api/ping/up"
+	shutServerPing         = "/api/ping/down"
+	pingTimeout            = 5 * time.Second
+	defaultTelemetry       = "true"
 )
 
 // App is the main application in the GoFr framework.
@@ -173,8 +178,16 @@ func (a *App) Run() {
 		shutdownCtx, done := context.WithTimeout(context.WithoutCancel(ctx), shutDownTimeout)
 		defer done()
 
+		if a.hasTelemetry() {
+			a.sendTelemetry(http.DefaultClient, false)
+		}
+
 		_ = a.Shutdown(shutdownCtx)
 	}()
+
+	if a.hasTelemetry() {
+		go a.sendTelemetry(http.DefaultClient, true)
+	}
 
 	wg := sync.WaitGroup{}
 
@@ -222,6 +235,37 @@ func (a *App) Run() {
 	}()
 
 	wg.Wait()
+}
+
+func (a *App) hasTelemetry() bool {
+	return a.Config.GetOrDefault("GOFR_TELEMETRY", defaultTelemetry) == "true"
+}
+
+func (a *App) sendTelemetry(client *http.Client, isStart bool) {
+	url := fmt.Sprint(gofrHost, shutServerPing)
+
+	if isStart {
+		url = fmt.Sprint(gofrHost, startServerPing)
+
+		a.container.Info("GoFr records the number of active servers. Set GOFR_TELEMETRY=false in configs to disable it.")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), pingTimeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, http.NoBody)
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Connection", "close")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	resp.Body.Close()
 }
 
 // Shutdown stops the service(s) and close the application.
