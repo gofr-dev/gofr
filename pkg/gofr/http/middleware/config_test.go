6b6dfd5bb99906011311d6a66ffcbbe3ad28ea1f
kevin.lockhart1001 <kevin.lockhart1001@proton.me>
2025-04-30 15:51:28 +0530

    Update middleware.config

--- pkg/gofr/http/middleware/config_test.go
+++ pkg/gofr/http/middleware/config_test.go
@@ -9,21 +9,47 @@ import (
 )
 
 func TestGetConfigs(t *testing.T) {
-	mockConfig := config.NewMockConfig(map[string]string{
-		"ACCESS_CONTROL_ALLOW_ORIGIN":       "*",
-		"ACCESS_CONTROL_ALLOW_HEADERS":      "Authorization, Content-Type",
-		"ACCESS_CONTROL_ALLOW_CREDENTIALS":  "true",
-		"ACCESS_CONTROL_ALLOW_CUSTOMHEADER": "abc",
-	})
+	// Without LOG_DISABLE_PROBES configs provided
+	{
+		mockConfig := config.NewMockConfig(map[string]string{
+			"ACCESS_CONTROL_ALLOW_ORIGIN":       "*",
+			"ACCESS_CONTROL_ALLOW_HEADERS":      "Authorization, Content-Type",
+			"ACCESS_CONTROL_ALLOW_CREDENTIALS":  "true",
+			"ACCESS_CONTROL_ALLOW_CUSTOMHEADER": "abc",
+		})
 
-	middlewareConfigs := GetConfigs(mockConfig)
+		middlewareConfigs := GetConfigs(mockConfig)
 
-	expectedConfigs := map[string]string{
-		"Access-Control-Allow-Origin":      "*",
-		"Access-Control-Allow-Headers":     "Authorization, Content-Type",
-		"Access-Control-Allow-Credentials": "true",
+		expectedConfigs := map[string]string{
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "Authorization, Content-Type",
+			"Access-Control-Allow-Credentials": "true",
+		}
+
+		assert.Equal(t, expectedConfigs, middlewareConfigs, "TestGetConfigs Failed!")
+		assert.NotContains(t, middlewareConfigs, "Access-Control-Allow-CustomHeader", "TestGetConfigs Failed!")
 	}
 
-	assert.Equal(t, expectedConfigs, middlewareConfigs, "TestGetConfigs Failed!")
-	assert.NotContains(t, middlewareConfigs, "Access-Control-Allow-CustomHeader", "TestGetConfigs Failed!")
+	// With LOG_DISABLE_PROBES config provided
+	{
+		mockConfig := config.NewMockConfig(map[string]string{
+			"ACCESS_CONTROL_ALLOW_ORIGIN":       "*",
+			"ACCESS_CONTROL_ALLOW_HEADERS":      "Authorization, Content-Type",
+			"ACCESS_CONTROL_ALLOW_CREDENTIALS":  "true",
+			"ACCESS_CONTROL_ALLOW_CUSTOMHEADER": "abc",
+			"LOG_DISABLE_PROBES":                "true",
+		})
+
+		middlewareConfigs := GetConfigs(mockConfig)
+
+		expectedConfigs := map[string]string{
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "Authorization, Content-Type",
+			"Access-Control-Allow-Credentials": "true",
+			"LOG_DISABLE_PROBES":               "true",
+		}
+
+		assert.Equal(t, expectedConfigs, middlewareConfigs, "TestGetConfigs Failed!")
+		assert.NotContains(t, middlewareConfigs, "Access-Control-Allow-CustomHeader", "TestGetConfigs Failed!")
+	}
 }
