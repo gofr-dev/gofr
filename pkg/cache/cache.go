faca11cac252718fcd7a268121712d41a9b89431
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-07-03 16:09:47 +0530

    feat(cache): Add Close method to Cache interface

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -2,8 +2,14 @@ package cache
 
 type Cache interface {
 	Get(key string) (interface{}, bool)
+
 	Set(key string, value interface{})
+
 	Delete(key string)
+
 	Exists(key string) bool
+
 	Clear()
+
+	Close()
 }
