61d1b1e4cc9870599b3ee86cbc2e284490736a31
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-07-06 12:20:20 +0530

    refactor(cache):  include context parameter for all methods

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,21 +1,23 @@
 package cache
 
+import "context"
+
 type Cache interface {
 	// Get returns the value associated with the key, a boolean indicating existence, and an error if any.
-	Get(key string) (interface{}, bool, error)
+	Get(ctx context.Context, key string) (interface{}, bool, error)
 
 	// Set stores the value for the key or returns an error.
-	Set(key string, value interface{}) error
+	Set(ctx context.Context, key string, value interface{}) error
 
 	// Delete removes the key or returns an error.
-	Delete(key string) error
+	Delete(ctx context.Context, key string) error
 
 	// Exists reports whether the key exists and hasn’t expired, or returns an error.
-	Exists(key string) (bool, error)
+	Exists(ctx context.Context, key string) (bool, error)
 
 	// Clear removes all items or returns an error.
-	Clear() error
+	Clear(ctx context.Context,) error
 
 	// Close stops background processes and marks the cache closed; returns error if already closed or on failure.
-	Close() error
+	Close(ctx context.Context,) error
 }
