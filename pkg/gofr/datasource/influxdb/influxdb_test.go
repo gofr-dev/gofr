14b30485631a5c981188a1086423f1d02aadf739
Akhilesh <akhilsharma3332@gmail.com>
2025-08-07 14:00:44 +0530

    [mock] health Success + Fail (both completed) Ping test completed.

--- pkg/gofr/datasource/influxdb/influxdb_test.go
+++ pkg/gofr/datasource/influxdb/influxdb_test.go
@@ -4,16 +4,15 @@ import (
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
+	"github.com/influxdata/influxdb-client-go/v2/domain"
+	"github.com/kataras/iris/v12/x/errors"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel"
 )
 
-func setupDB(t *testing.T) *Client {
+func setupDB(t *testing.T, ctrl *gomock.Controller) *Client {
 	t.Helper()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	// Setup
 	mockLogger := NewMockLogger(ctrl)
 	mockMetrics := NewMockMetrics(ctrl)
 	mockInfluxClient := NewMockInfluxClient(ctrl)
@@ -32,7 +31,6 @@ func setupDB(t *testing.T) *Client {
 	client.UseTracer(otel.GetTracerProvider().Tracer("gofr-influxdb"))
 
 	mockLogger.EXPECT().Debugf(gomock.Any(), gomock.Any()).AnyTimes()
-	// mockMetrics.EXPECT().Errorf(gomock.Any(), gomock.Any(), gomock.Any())
 
 	// Replace the client with our mocked version
 	client.client = mockInfluxClient
@@ -45,10 +43,74 @@ func Test_HelthCheckSuccess(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	// mockInflux := NewMockInfluxDB(ctrl)
-	client := *setupDB(t)
-	client.HealthCheck(t.Context())
+	client := *setupDB(t, ctrl)
+	mockInflux := client.client.(*MockInfluxClient)
 
-	// client.HealthCheck(t.Context())
+	expectedHealth := &domain.HealthCheck{Status: "pass"}
+	mockInflux.EXPECT().
+		Health(gomock.Any()).
+		Return(expectedHealth, nil).
+		Times(1)
 
+	_, err := client.HealthCheck(t.Context())
+	require.NoError(t, err)
+}
+
+func Test_HelthCheckFail(t *testing.T) {
+	t.Helper()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := *setupDB(t, ctrl)
+	mockInflux := client.client.(*MockInfluxClient)
+
+	expectedHealth := &domain.HealthCheck{Status: "fail"}
+	mockInflux.EXPECT().
+		Health(gomock.Any()).
+		Return(expectedHealth, errors.New("No influxdb found")).
+		Times(1)
+
+	_, err := client.HealthCheck(t.Context())
+	require.Error(t, err)
+}
+
+func Test_PingSuccess(t *testing.T) {
+	t.Helper()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := *setupDB(t, ctrl)
+	mockInflux := client.client.(*MockInfluxClient)
+
+	mockInflux.EXPECT().
+		Ping(gomock.Any()).
+		Return(true, nil).
+		Times(1)
+
+	health, err := client.Ping(t.Context())
+
+	require.NoError(t, err) // empty organization name
+	require.True(t, health)
+}
+
+func Test_PingFailed(t *testing.T) {
+	t.Helper()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := *setupDB(t, ctrl)
+	mockInflux := client.client.(*MockInfluxClient)
+
+	mockInflux.EXPECT().
+		Ping(gomock.Any()).
+		Return(false, errors.New("Something Unexptected")).
+		Times(1)
+
+	health, err := client.Ping(t.Context())
+
+	require.Error(t, err) // empty organization name
+	require.False(t, health)
 }
