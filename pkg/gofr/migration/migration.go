492e1718c7258b78ba26c4794d2ae210ee68268c
coolwednesday <divi229moc@gmail.com>
2025-08-11 23:45:17 +0530

    revert extra changes

--- pkg/gofr/migration/migration.go
+++ pkg/gofr/migration/migration.go
@@ -37,7 +37,6 @@ func Run(migrationsMap map[int64]Migrate, c *container.Container) {
 
 	ds, mg, ok := getMigrator(c)
 	ds.Logger = c.Logger
-	ds.Services = c.Services
 	
 	// Returning with an error log as migration would eventually fail as No databases are initialized.
 	// Pub/Sub is considered as initialized if its configurations are given.
