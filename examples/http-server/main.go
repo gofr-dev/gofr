65c0e3ffdb76a987db9b2ef33509d527df10572b
coolwednesday <divi229moc@gmail.com>
2025-08-11 08:12:58 +0530

    async logging

--- examples/http-server/main.go
+++ examples/http-server/main.go
@@ -1,77 +1,102 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-
+	"go.opentelemetry.io/otel/trace"
 	"gofr.dev/pkg/gofr"
-	"gofr.dev/pkg/gofr/datasource/couchbase"
 )
 
-type User struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
 func main() {
-	// Create a new GoFr application
+	// Create a new application
 	a := gofr.New()
 
-	// Add the Couchbase datasource to the application
-	a.AddCouchbase(couchbase.New(&couchbase.Config{
-		Host:     "localhost",
-		User:     "Administrator",
-		Password: "password",
-		Bucket:   "test-bucket",
-	}))
+	//HTTP service with default health check endpoint
+	a.AddHTTPService("anotherService", "http://localhost:9000")
 
-	// Add the routes
-	a.GET("/users/{id}", getUser)
-	a.POST("/users", createUser)
-	a.DELETE("/users/{id}", deleteUser)
+	// Add all the routes
+	a.GET("/hello", HelloHandler)
+	//a.GET("/error", ErrorHandler)
+	//a.GET("/redis", RedisHandler)
+	//a.GET("/trace", TraceHandler)
+	//a.GET("/mysql", MysqlHandler)
 
 	// Run the application
 	a.Run()
 }
 
-func getUser(c *gofr.Context) (any, error) {
-	// Get the user ID from the URL path
-	id := c.PathParam("id")
-
-	// Get the user from Couchbase
-	var user User
-	if err := c.Couchbase.Get(c, id, &user); err != nil {
-		return nil, err
+func HelloHandler(c *gofr.Context) (any, error) {
+	name := c.Param("name")
+	if name == "" {
+		c.Log("Name came empty")
+		name = "World"
 	}
 
-	return user, nil
+	traceID := trace.SpanFromContext(c).SpanContext().TraceID().String()
+
+	return traceID, nil
 }
 
-func createUser(c *gofr.Context) (any, error) {
-	// Get the user from the request body
-	var user User
-	if err := c.Bind(&user); err != nil {
-		return nil, err
-	}
-
-	// Insert the user into Couchbase
-	if err := c.Couchbase.Insert(c, user.ID, user, nil); err != nil {
-		return nil, err
-	}
-
-	return "user created successfully", nil
-}
-
-func deleteUser(c *gofr.Context) (any, error) {
-	// Get the user ID from the URL path
-	id := c.PathParam("id")
-
-	// Remove the user from Couchbase
-	if err := c.Couchbase.Remove(c, id); err != nil {
-		return nil, err
-	}
-
-	return "user deleted successfully", nil
-}
+//
+//func ErrorHandler(c *gofr.Context) (any, error) {
+//	return nil, errors.New("some error occurred")
+//}
+//
+//func RedisHandler(c *gofr.Context) (any, error) {
+//	val, err := c.Redis.Get(c, "test").Result()
+//	if err != nil && err != redis.Nil { // If key is not found, we are not considering this an error and returning "".
+//		return nil, datasource.ErrorDB{Err: err, Message: "error from redis db"}
+//	}
+//
+//	return val, nil
+//}
+//
+//func TraceHandler(c *gofr.Context) (any, error) {
+//	defer c.Trace("traceHandler").End()
+//
+//	span2 := c.Trace("some-sample-work")
+//	<-time.After(time.Millisecond * 1) //nolint:wsl    // Waiting for 1ms to simulate workload
+//	defer span2.End()
+//
+//	// Ping redis 5 times concurrently and wait.
+//	count := 5
+//	wg := sync.WaitGroup{}
+//	wg.Add(count)
+//
+//	for i := 0; i < count; i++ {
+//		go func() {
+//			c.Redis.Ping(c)
+//			wg.Done()
+//		}()
+//	}
+//	wg.Wait()
+//
+//	//Call to Another service
+//	resp, err := c.GetHTTPService("anotherService").Get(c, "redis", nil)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	defer resp.Body.Close()
+//
+//	var data = struct {
+//		Data any `json:"data"`
+//	}{}
+//
+//	b, err := io.ReadAll(resp.Body)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	_ = json.Unmarshal(b, &data)
+//
+//	return data.Data, nil
+//}
+//
+//func MysqlHandler(c *gofr.Context) (any, error) {
+//	var value int
+//	err := c.SQL.QueryRowContext(c, "select 2+2").Scan(&value)
+//	if err != nil {
+//		return nil, datasource.ErrorDB{Err: err, Message: "error from sql db"}
+//	}
+//
+//	return value, nil
+//}
