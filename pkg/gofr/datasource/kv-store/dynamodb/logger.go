af39688f9191326218c443a4c2d096499a999355
coolwednesday <divi229moc@gmail.com>
2025-09-07 19:16:14 +0530

    revert extra changes

--- pkg/gofr/datasource/kv-store/dynamodb/logger.go
+++ pkg/gofr/datasource/kv-store/dynamodb/logger.go
@@ -1,5 +1,10 @@
 package dynamodb
 
+import (
+	"fmt"
+	"io"
+)
+
 type Logger interface {
 	Debug(args ...any)
 	Debugf(pattern string, args ...any)
@@ -14,5 +19,10 @@ type Log struct {
 	Duration int64  `json:"duration"`
 	Key      string `json:"key"`
 	Value    string `json:"value,omitempty"`
+}
+
+func (l *Log) PrettyPrint(writer io.Writer) {
+	fmt.Fprintf(writer, "\u001B[38;5;8m%-32s \u001B[38;5;162m%-6s\u001B[0m %8d\u001B[38;5;8mµs\u001B[0m %s \n",
+		l.Type, "DYNMO", l.Duration, l.Key+" "+l.Value)
 }
 
