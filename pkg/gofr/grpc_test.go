1a0e09569477fb18622d74f33c6c73e6adf6c2c9
Ayush Jayaswal <ayushjayaswal@yandex.com>
2025-09-12 09:21:37 +0530

    typo

--- pkg/gofr/grpc_test.go
+++ pkg/gofr/grpc_test.go
@@ -72,7 +72,7 @@ func createTestInterceptors() []grpc.UnaryServerInterceptor {
 	}
 }
 
-//nolint:thelper // linter is suppressed to avoid SA5011 (possible nil pointer derefernce) warnings from staticcheck
+//nolint:thelper // linter is suppressed to avoid SA5011 (possible nil pointer dereference) warnings from staticcheck
 func createMockGRPCConfig(t *testing.T, port int, enableReflection bool) config.Config {
 	// Use a default metrics port if t is nil (for cases where we don't need a real free port)
 	metricsPort := 8080
