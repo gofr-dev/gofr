f56364d14ae6273abb35b0dd8a52ed1e5a034b0f
Umang01-hash <mundhraumang.02@gmail.com>
2025-09-17 12:07:05 +0530

    fix linters

--- pkg/gofr/service/rate_limiter_test.go
+++ pkg/gofr/service/rate_limiter_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel"
+
 	gofrRedis "gofr.dev/pkg/gofr/datasource/redis"
 	"gofr.dev/pkg/gofr/logging"
 )
