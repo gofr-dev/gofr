0e867fee540ac58588ad655c32ae5885d41c6da9
Umang01-hash <mundhraumang.02@gmail.com>
2025-09-26 17:47:49 +0530

    update telemetry endpoint

--- pkg/gofr/metrics/exporters/telemetry.go
+++ pkg/gofr/metrics/exporters/telemetry.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	defaultTelemetryEndpoint = "http://localhost:8080/v1/metrics"
+	defaultTelemetryEndpoint = "https://gofr.dev/telemetry/v1/metrics"
 	defaultAppName           = "gofr-app"
 	requestTimeout           = 10 * time.Second
 )
