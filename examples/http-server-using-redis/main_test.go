35394c7d007bfe12031489e6f6c1d4fa6dc56de0
Akshay Cdr <akshaychandra418@gmail.com>
2025-10-13 00:22:16 +0530

    added tests for Redis Get handler and cache warmup functionality

--- examples/http-server-using-redis/main_test.go
+++ examples/http-server-using-redis/main_test.go
@@ -87,6 +87,65 @@ func TestRedisSetHandler(t *testing.T) {
 	require.Error(t, err)
 }
 
+func TestRedisGetHandler(t *testing.T) {
+	configs := testutil.NewServerConfigs(t)
+
+	a := gofr.New()
+	logger := logging.NewLogger(logging.DEBUG)
+	redisClient, mock := redismock.NewClientMock()
+
+	rc := redis.NewClient(config.NewMockConfig(map[string]string{"REDIS_HOST": "localhost", "REDIS_PORT": "2001"}), logger, a.Metrics())
+	rc.Client = redisClient
+
+	mock.ExpectSet("testKey1", "testValue1", time.Minute*5).SetErr(testutil.CustomError{ErrorMessage: "redis get error"})
+	mock.ClearExpect()
+
+	mock.ExpectGet("testKey1").SetErr(testutil.CustomError{ErrorMessage: "redis get error"})
+	mock.ClearExpect()
+
+	req, _ := http.NewRequestWithContext(context.Background(), http.MethodGet, fmt.Sprintf("http://localhost:%d/handle", configs.HTTPPort), bytes.NewBuffer([]byte(`{"key":"value"}`)))
+	req.Header.Set("content-type", "application/json")
+	gofrReq := gofrHTTP.NewRequest(req)
+
+	ctx := &gofr.Context{Context: context.Background(),
+		Request: gofrReq, Container: &container.Container{Logger: logger, Redis: rc}}
+
+	resp, err := RedisGetHandler(ctx)
+
+	assert.Nil(t, resp)
+	require.Error(t, err)
+}
+
+func TestWarmupCache_Success(t *testing.T) {
+	logger := logging.NewLogger(logging.DEBUG)
+	redisClient, mock := redismock.NewClientMock()
+	mock.ExpectSet("initial-data", "This is some data cached at startup.", 0).SetVal("OK")
+
+	rc := &redis.Redis{Client: redisClient}
+
+	ctx := &gofr.Context{Context: context.Background(), Container: &container.Container{Logger: logger, Redis: rc}}
+
+	err := warmupCache(ctx)
+	require.NoError(t, err)
+}
+
+func TestWarmupCache_Failure(t *testing.T) {
+
+	logger := logging.NewLogger(logging.DEBUG)
+	redisClient, mock := redismock.NewClientMock()
+	mock.ExpectSet("initial-data", "This is some data cached at startup.", 0).
+		SetErr(fmt.Errorf("mocked redis error"))
+
+	rc := &redis.Redis{Client: redisClient}
+
+	rc.Client = redisClient
+
+	ctx := &gofr.Context{Context: context.Background(), Container: &container.Container{Logger: logger, Redis: rc}}
+
+	err := warmupCache(ctx)
+	require.Error(t, err)
+}
+
 func TestRedisPipelineHandler(t *testing.T) {
 	configs := testutil.NewServerConfigs(t)
 
