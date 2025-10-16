a878c4fb82fa6cfa01e13a22df9a3797a17ce86f
0xEbrahim <elsayedebrahim603@gmail.com>
2025-10-16 20:10:25 +0300

    refactor: auth service

--- pkg/gofr/service/auth.go
+++ pkg/gofr/service/auth.go
@@ -12,18 +12,33 @@ type authProvider struct {
 	HTTP
 }
 
+type WithHeaderType func(ctx context.Context, path string, queryParams map[string]any, body []byte,
+	headers map[string]string) (*http.Response, error)
+
+func (a *authProvider) doAuthWithHeaders(
+	ctx context.Context,
+	withHeader WithHeaderType,
+	path string,
+	queryParams map[string]any,
+	body []byte,
+	headers map[string]string,
+) (*http.Response, error) {
+	headers, err := a.auth(ctx, headers)
+	if err != nil {
+		return nil, err
+	}
+	return withHeader(ctx, path, queryParams, body, headers)
+}
+
 func (a *authProvider) Get(ctx context.Context, path string, queryParams map[string]any) (*http.Response, error) {
 	return a.GetWithHeaders(ctx, path, queryParams, nil)
 }
 
 func (a *authProvider) GetWithHeaders(ctx context.Context, path string, queryParams map[string]any,
 	headers map[string]string) (*http.Response, error) {
-	headers, err := a.auth(ctx, headers)
-	if err != nil {
-		return nil, err
-	}
-
-	return a.HTTP.GetWithHeaders(ctx, path, queryParams, headers)
+	return a.doAuthWithHeaders(ctx, func(ctx context.Context, path string, queryParams map[string]any, _ []byte, headers map[string]string) (*http.Response, error) {
+		return a.HTTP.GetWithHeaders(ctx, path, queryParams, headers)
+	}, path, queryParams, nil, headers)
 }
 
 func (a *authProvider) Post(ctx context.Context, path string, queryParams map[string]any,
@@ -33,12 +48,7 @@ func (a *authProvider) Post(ctx context.Context, path string, queryParams map[st
 
 func (a *authProvider) PostWithHeaders(ctx context.Context, path string, queryParams map[string]any,
 	body []byte, headers map[string]string) (*http.Response, error) {
-	headers, err := a.auth(ctx, headers)
-	if err != nil {
-		return nil, err
-	}
-
-	return a.HTTP.PostWithHeaders(ctx, path, queryParams, body, headers)
+	return a.doAuthWithHeaders(ctx, a.HTTP.PostWithHeaders, path, queryParams, body, headers)
 }
 
 func (a *authProvider) Patch(ctx context.Context, path string, queryParams map[string]any, body []byte) (*http.Response, error) {
@@ -47,12 +57,7 @@ func (a *authProvider) Patch(ctx context.Context, path string, queryParams map[s
 
 func (a *authProvider) PatchWithHeaders(ctx context.Context, path string, queryParams map[string]any,
 	body []byte, headers map[string]string) (*http.Response, error) {
-	headers, err := a.auth(ctx, headers)
-	if err != nil {
-		return nil, err
-	}
-
-	return a.HTTP.PatchWithHeaders(ctx, path, queryParams, body, headers)
+	return a.doAuthWithHeaders(ctx, a.HTTP.PatchWithHeaders, path, queryParams, body, headers)
 }
 
 func (a *authProvider) Put(ctx context.Context, path string, queryParams map[string]any,
@@ -62,12 +67,7 @@ func (a *authProvider) Put(ctx context.Context, path string, queryParams map[str
 
 func (a *authProvider) PutWithHeaders(ctx context.Context, path string, queryParams map[string]any,
 	body []byte, headers map[string]string) (*http.Response, error) {
-	headers, err := a.auth(ctx, headers)
-	if err != nil {
-		return nil, err
-	}
-
-	return a.HTTP.PutWithHeaders(ctx, path, queryParams, body, headers)
+	return a.doAuthWithHeaders(ctx, a.HTTP.PutWithHeaders, path, queryParams, body, headers)
 }
 
 func (a *authProvider) Delete(ctx context.Context, path string, body []byte) (*http.Response, error) {
@@ -75,10 +75,7 @@ func (a *authProvider) Delete(ctx context.Context, path string, body []byte) (*h
 }
 
 func (a *authProvider) DeleteWithHeaders(ctx context.Context, path string, body []byte, headers map[string]string) (*http.Response, error) {
-	headers, err := a.auth(ctx, headers)
-	if err != nil {
-		return nil, err
-	}
-
-	return a.HTTP.DeleteWithHeaders(ctx, path, body, headers)
+	return a.doAuthWithHeaders(ctx, func(ctx context.Context, path string, _ map[string]any, body []byte, headers map[string]string) (*http.Response, error) {
+		return a.HTTP.DeleteWithHeaders(ctx, path, body, headers)
+	}, path, nil, body, headers)
 }
