529df48cb17a7d9329ab8e5ecd7e6f4b21950fc0
0xEbrahim <elsayedebrahim603@gmail.com>
2025-10-17 08:55:11 +0300

    fix: linting

--- pkg/gofr/service/auth.go
+++ pkg/gofr/service/auth.go
@@ -27,18 +27,21 @@ func (a *authProvider) doAuthWithHeaders(
 	if err != nil {
 		return nil, err
 	}
+
 	return withHeader(ctx, path, queryParams, body, headers)
 }
 
-func (a *authProvider) Get(ctx context.Context, path string, queryParams map[string]any) (*http.Response, error) {
+func (a *authProvider) Get(ctx context.Context,
+	path string, queryParams map[string]any) (*http.Response, error) {
 	return a.GetWithHeaders(ctx, path, queryParams, nil)
 }
 
 func (a *authProvider) GetWithHeaders(ctx context.Context, path string, queryParams map[string]any,
 	headers map[string]string) (*http.Response, error) {
-	return a.doAuthWithHeaders(ctx, func(ctx context.Context, path string, queryParams map[string]any, _ []byte, headers map[string]string) (*http.Response, error) {
-		return a.HTTP.GetWithHeaders(ctx, path, queryParams, headers)
-	}, path, queryParams, nil, headers)
+	return a.doAuthWithHeaders(ctx,
+		func(ctx context.Context, path string, queryParams map[string]any, _ []byte, headers map[string]string) (*http.Response, error) {
+			return a.HTTP.GetWithHeaders(ctx, path, queryParams, headers)
+		}, path, queryParams, nil, headers)
 }
 
 func (a *authProvider) Post(ctx context.Context, path string, queryParams map[string]any,
@@ -75,7 +78,8 @@ func (a *authProvider) Delete(ctx context.Context, path string, body []byte) (*h
 }
 
 func (a *authProvider) DeleteWithHeaders(ctx context.Context, path string, body []byte, headers map[string]string) (*http.Response, error) {
-	return a.doAuthWithHeaders(ctx, func(ctx context.Context, path string, _ map[string]any, body []byte, headers map[string]string) (*http.Response, error) {
-		return a.HTTP.DeleteWithHeaders(ctx, path, body, headers)
-	}, path, nil, body, headers)
+	return a.doAuthWithHeaders(ctx,
+		func(ctx context.Context, path string, _ map[string]any, body []byte, headers map[string]string) (*http.Response, error) {
+			return a.HTTP.DeleteWithHeaders(ctx, path, body, headers)
+		}, path, nil, body, headers)
 }
